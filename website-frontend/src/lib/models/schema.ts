7c62ffe681af198b5d843ee1e73d4da6f89af673
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-10-14 23:41:43 +0800

    fix: prefer Events over explicitly parsing into an array of Event

--- website-frontend/src/lib/models/schema.ts
+++ website-frontend/src/lib/models/schema.ts
@@ -1,10 +1,10 @@
-import { array, object, type InferOutput } from 'valibot';
-import { Event } from './event';
+import { object, type InferOutput } from 'valibot';
+import { Events } from './event';
 import { Global } from './global';
 
 export const Schema = object({
 	global: Global,
-	events: array(Event)
+	events: Events
 });
 
 export type Schema = InferOutput<typeof Schema>;
