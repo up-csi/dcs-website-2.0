885926892478c5a7673cbc5d86593a78382110f4
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-22 07:23:58 +0800

    fix(frontend): catch empty favicon link

--- website-frontend/src/lib/directus.ts
+++ website-frontend/src/lib/directus.ts
@@ -2,7 +2,7 @@ import { createDirectus, rest } from '@directus/sdk';
 import { PUBLIC_APIURL } from '$env/static/public';
 import { type Schema } from './models/schema';
 
-function getDirectusInstance(fetch) {
+function getDirectusInstance(fetch: any) {
 	const options = fetch ? { globals: { fetch } } : {};
 	const directus = createDirectus<Schema>(PUBLIC_APIURL, options).with(rest());
 	return directus;
--- website-frontend/src/lib/models/global.ts
+++ website-frontend/src/lib/models/global.ts
@@ -1,9 +1,9 @@
-import { object, string, type InferOutput } from 'valibot';
+import { nullish, object, string, type InferOutput } from 'valibot';
 
 export const Global = object({
 	title: string(),
 	description: string(),
-	favicon: string()
+	favicon: nullish(string())
 });
 
 export type Global = InferOutput<typeof Global>;
