ab1a1f5e0f7db19530dafec49bd72aaacf9bb181
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2024-11-25 12:34:44 +0800

    fix(frontend): add HTML sanitizer in `student_council.ts`

--- website-frontend/src/lib/models/student_council.ts
+++ website-frontend/src/lib/models/student_council.ts
@@ -1,7 +1,8 @@
-import { object, string, type InferOutput } from 'valibot';
+import { cleanHtml } from '$lib/models-helpers';
+import { object, pipe, string, type InferOutput } from 'valibot';
 
 export const StudentCouncil = object({
-	flexible_content: string()
+	flexible_content: pipe(string(), cleanHtml)
 });
 
 export type StudentCouncil = InferOutput<typeof StudentCouncil>;
