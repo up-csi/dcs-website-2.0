0b030056e63a51daf61f3b764fa30609f337e1a7
Clarisse Bianca Bucu <isaybucu@gmail.com>
2024-11-27 18:45:54 +0800

    fix: add proper route parametrization for academics pages

---
+++ website-frontend/src/routes/academics/+page.server.ts
@@ -0,0 +1,12 @@
+const programs = [
+    {slug: "bs-cs", title:"Bachelor of Science in Computer Science", image:"...", description: "owo", curriculum: "insert curriculum table here"},
+    {slug: "ms-cs", title:"Master of Science in Computer Science", image:"...", description: "owo", curriculum: "insert curriculum table here"},
+    {slug: "ms-bioinformatics", title:"Master of Science in Bioinformatics", image:"...", description: "owo", curriculum: "insert curriculum table here"},
+    {slug: "phd-cs", title:"Doctor of Philosophy in Computer Science", image:"...", description: "owo", curriculum: "insert curriculum table here"},
+    ];
+
+export function load() {
+    return {
+        programs: programs
+    }
+}
--- website-frontend/src/routes/academics/+page.svelte
+++ website-frontend/src/routes/academics/+page.svelte
@@ -1,18 +1,11 @@
 <script>
-    let academics = [
-        {slug: "bs-cs", title:"Bachelor of Science in Computer Science", image:"...", description: "owo"},
-        {slug: "ms-cs", title:"Master of Science in Computer Science", image:"...", description: "owo"},
-        {slug: "ms-bioinformatics", title:"Master of Science in Bioinformatics", image:"...", description: "owo"},
-        {slug: "phd-cs", title:"Doctor of Philosophy in Computer Science", image:"...", description: "owo"},
-        ]
+    export let data; 
 </script>
 
-<div class="text-white">
-    <h1>Academics</h1>
+<h1>Academics</h1>
 
-    <ul>
-    	{#each academics as { slug, title }}
-    		<li><a href="/academics/{slug}">{title}</a></li>
-    	{/each}
-    </ul>
-</div>
+<ul>
+	{#each data.programs as { slug, title }}
+		<li><a href="/academics/{slug}">{title}</a></li>
+	{/each}
+</ul>
