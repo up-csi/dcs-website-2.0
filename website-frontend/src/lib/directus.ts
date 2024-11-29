7919696f4e7abb36c18fa047108899716e0f60e5
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-29 15:26:50 +0800

    fix: explicitly define type of fetch function

--- website-frontend/src/lib/directus.ts
+++ website-frontend/src/lib/directus.ts
@@ -1,8 +1,9 @@
 import { createDirectus, rest } from '@directus/sdk';
 import { PUBLIC_APIURL } from '$env/static/public';
 import { type Schema } from './models/schema';
+import type { Fetch } from './fetch';
 
-function getDirectusInstance(fetch) {
+function getDirectusInstance(fetch: Fetch | null) {
 	const options = fetch ? { globals: { fetch } } : {};
 	const directus = createDirectus<Schema>(PUBLIC_APIURL, options).with(rest());
 	return directus;
