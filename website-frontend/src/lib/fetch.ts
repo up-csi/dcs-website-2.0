3208c8e0e40959b8b99b6c5062e28697b3e028cd
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-29 15:28:46 +0800

    chore: run linter

--- website-frontend/src/lib/fetch.ts
+++ website-frontend/src/lib/fetch.ts
@@ -1,4 +1,4 @@
 export type Fetch = {
-    (input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
-    (input: string | URL | globalThis.Request, init?: RequestInit): Promise<Response>;
-}
+	(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+	(input: string | URL | globalThis.Request, init?: RequestInit): Promise<Response>;
+};
