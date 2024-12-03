f68af2431860b206b7151fefbbb980df735d1587
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-03 20:56:15 +0800

    feat(NavBar): make component for navbar items

---
+++ website-frontend/src/lib/components/nav/NavItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let href: string, to: string;
+    let show = false;
+</script>
+
+<li 
+    class="relative"
+    on:pointerenter={() => {show = true}} 
+    on:pointerleave={() => {show = false}}
+>
+    <a {href}>{to}</a>
+    {#if show}
+        <ul
+            class="absolute w-40" 
+        >
+            <slot />
+        </ul>
+    {/if}
+</li>
