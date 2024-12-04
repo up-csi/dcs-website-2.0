d48e4204896741e29e58d4badae617b7f0644084
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2024-12-04 17:25:47 +0800

    feat(schema): add `Alumni` model in schema

--- website-frontend/src/lib/models/schema.ts
+++ website-frontend/src/lib/models/schema.ts
@@ -2,11 +2,13 @@ import { object, type InferOutput } from 'valibot';
 import { Events } from './event';
 import { Global } from './global';
 import { StudentCouncil } from './student_council';
+import { Alumni } from './alumni';
 
 export const Schema = object({
 	global: Global,
 	events: Events,
-	student_council: StudentCouncil
+	student_council: StudentCouncil,
+	alumni: Alumni
 });
 
 export type Schema = InferOutput<typeof Schema>;
