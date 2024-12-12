6717358c56c77c6eae9ae3b88d3264bca8d277bf
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-13 00:12:05 +0800

    fix: revise home link test

--- website-frontend/tests/layout.spec.ts
+++ website-frontend/tests/layout.spec.ts
@@ -2,10 +2,11 @@ import { test, expect } from "@playwright/test";
 
 test.describe('2-4: Navigation Bar Links to Home', () => {
     test('Check for home button in navbar', async ({ page }) => {
-        await page.goto("http://localhost:5173");
+        await page.goto("/");
         const nav = await page.getByRole('navigation');
         const home = await nav.getByRole('link', {name: 'Home'});
         await home.click();
-        expect(page.url()).toBe("http://localhost:5173/");
+        await page.waitForTimeout(5000);
+        expect(await page.title()).toBe("UPD DCS");
     })
 })
