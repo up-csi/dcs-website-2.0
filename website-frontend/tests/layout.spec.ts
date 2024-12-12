07a49d526ed8f40a3514022efbc20e2b8cfde335
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-13 00:16:22 +0800

    feat: add placeholders for other tests

--- website-frontend/tests/layout.spec.ts
+++ website-frontend/tests/layout.spec.ts
@@ -10,3 +10,19 @@ test.describe('2-4: Navigation Bar Links to Home', () => {
         expect(await page.title()).toBe("UPD DCS");
     })
 })
+
+test.describe('2-5: Navigation Bar Links to Events', () => {
+
+})
+
+test.describe('2-6: Navigation Bar Links to Alumni', () => {
+
+})
+
+test.describe('2-7: Navigation Bar Links to People', () =>{
+
+})
+
+test.describe('2-12: Navigation Bar Links to About, with dropdowns to subpages', () => {
+
+})
