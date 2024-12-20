842c93fa2445f6c8e3ceebe198a56a3b42ab12a0
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:03:37 +0800

    feat(Events): prefer featured event card component

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import FeaturedEventCard from "$lib/components/events/FeaturedEventCard.svelte";
 	/** @type {import('./$types').PageData} */
 
 	export let data;
@@ -18,13 +19,7 @@
 		</div>
 		<div class="flex my-12 space-x-8">
 			{#each events as event}
-				<div class="card p-4 w-96 h-48">
-					<div class="flex justify-between mb-4">
-						<h3>{event.event_headline}</h3>
-						<h3>{new Date(event.date_created).toLocaleDateString()}</h3>
-					</div>
-					{@html event.event_content}
-				</div>
+				<FeaturedEventCard {event} />
 			{/each}
 		</div>
 	</div>
--- website-frontend/src/routes/events/+page.svelte
+++ website-frontend/src/routes/events/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import FeaturedEventCard from "$lib/components/events/FeaturedEventCard.svelte";
 	/** @type {import('./$types').PageData} */
 
 	export let data;
@@ -9,13 +10,7 @@
 <div class="container h-full mx-auto flex-col justify-center items-center my-8">
 	<div class="flex my-12 space-x-8">
         {#each events as event}
-            <div class="card p-4 w-96 h-48">
-                <div class="flex justify-between mb-4">
-                    <h3>{event.event_headline}</h3>
-                    <h3>{new Date(event.date_created).toLocaleDateString()}</h3>
-                </div>
-                {@html event.event_content}
-            </div>
+            <FeaturedEventCard {event} />
         {/each}
     </div>
 </div>
