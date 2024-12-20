f3e36f0e6c62f51eda113b84a230f62c79f67003
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:37:10 +0800

    chore: run linters, formatters, and builders

--- website-frontend/src/lib/components/events/FeaturedEventCard.svelte
+++ website-frontend/src/lib/components/events/FeaturedEventCard.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import { Event } from "$lib/models/event";
-    export let event: Event;
+	import { Event } from '$lib/models/event';
+	export let event: Event;
 </script>
 
 <div class="card p-4 w-72 max-w-72 md:w-96 md:max-w-96 h-fit snap-center">
-    <div class="md:flex md:justify-between mb-4">
-        <h3>{event.event_headline}</h3>
-        <h3>{new Date(event.date_created).toLocaleDateString()}</h3>
-    </div>
-    <div class="max-h-6 *:truncate overflow-hidden">
-        {@html event.event_content}
-    </div>
-    <a>View details &#8594;</a>
+	<div class="md:flex md:justify-between mb-4">
+		<h3>{event.event_headline}</h3>
+		<h3>{new Date(event.date_created).toLocaleDateString()}</h3>
+	</div>
+	<div class="max-h-6 *:truncate overflow-hidden">
+		{@html event.event_content}
+	</div>
+	<p>View details &#8594;</p>
 </div>
