3434984bcda89e3f2316790903fbc16ab3319011
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:18:51 +0800

    feat(Events): show all events

--- website-frontend/src/routes/events/+page.svelte
+++ website-frontend/src/routes/events/+page.svelte
@@ -17,4 +17,26 @@
             <FeaturedEventCard {event} />
         {/each}
     </div>
+    <div>
+        {#each events as event}
+            <div class="grid grid-cols-6 border rounded p-4">
+                <div class="col-span-5">
+                    <h3>{event.event_headline}</h3>
+                    <div class="max-h-6 *:truncate overflow-hidden">
+                        {@html event.event_content}
+                    </div>
+                    <a>View details &#8594;</a>
+                    <div class="flex items-center">
+                        <p>Tags: </p>
+                        {#each event.tags as tag}
+                            <p class="border rounded px-2 py-1 mx-2">{tag}</p>
+                        {/each}
+                    </div>
+                </div>
+                <div class="place-content-center">
+                    <h3>{new Date(event.date_created).toLocaleDateString()}</h3>
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
