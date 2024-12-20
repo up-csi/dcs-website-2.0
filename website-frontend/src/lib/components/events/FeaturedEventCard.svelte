1f51e992f3c564d7f973b043ac56e1106778c942
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:07:13 +0800

    feat(FeaturedEventCard): add link to details page

--- website-frontend/src/lib/components/events/FeaturedEventCard.svelte
+++ website-frontend/src/lib/components/events/FeaturedEventCard.svelte
@@ -8,5 +8,8 @@
         <h3>{event.event_headline}</h3>
         <h3>{new Date(event.date_created).toLocaleDateString()}</h3>
     </div>
-    {@html event.event_content}
+    <div class="max-h-6 *:truncate overflow-hidden">
+        {@html event.event_content}
+    </div>
+    <a>View details &#8594;</a>
 </div>
