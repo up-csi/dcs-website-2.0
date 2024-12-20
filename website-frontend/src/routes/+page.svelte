22ca877f9bcddeab064e6ab42a5abf88045ed590
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:11:19 +0800

    feat(Events): show up to 3 featured events

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    import { Events } from "$lib/models/event";
 	import FeaturedEventCard from "$lib/components/events/FeaturedEventCard.svelte";
-	/** @type {import('./$types').PageData} */
 
+	/** @type {import('./$types').PageData} */
 	export let data;
 
 	$: ({ global, events } = data);
+
+	let featured: Events = [];
+    $: featured = events?.slice(0, 3);
 </script>
 
 <div class="container mx-auto my-8 h-full flex-col items-center justify-center">
@@ -18,7 +22,7 @@
 			<a href="/events">View all &#8594;</a>
 		</div>
 		<div class="flex my-12 space-x-8">
-			{#each events as event}
+			{#each featured as event}
 				<FeaturedEventCard {event} />
 			{/each}
 		</div>
--- website-frontend/src/routes/events/+page.svelte
+++ website-frontend/src/routes/events/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+    import { Events } from "$lib/models/event";
     import FeaturedEventCard from "$lib/components/events/FeaturedEventCard.svelte";
-	/** @type {import('./$types').PageData} */
 
+	/** @type {import('./$types').PageData} */
 	export let data;
 
 	$: ({ events } = data);
+
+    let featured: Events = [];
+    $: featured = events?.slice(0, 3);
 </script>
 
 <div class="container h-full mx-auto flex-col justify-center items-center my-8">
 	<div class="flex my-12 space-x-8">
-        {#each events as event}
+        {#each featured as event}
             <FeaturedEventCard {event} />
         {/each}
     </div>
