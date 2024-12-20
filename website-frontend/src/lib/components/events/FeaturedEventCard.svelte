8c8eec3a88c4cacc1e94cff5a245847e1b4214da
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:03:37 +0800

    feat(Events): prefer featured event card component

---
+++ website-frontend/src/lib/components/events/FeaturedEventCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Event } from "$lib/models/event";
+    export let event: Event;
+</script>
+
+<div class="card p-4 w-96 h-48">
+    <div class="flex justify-between mb-4">
+        <h3>{event.event_headline}</h3>
+        <h3>{new Date(event.date_created).toLocaleDateString()}</h3>
+    </div>
+    {@html event.event_content}
+</div>
