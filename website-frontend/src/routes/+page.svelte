c39e5a312f728b60a09aad68f6d3e6259bdac44c
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-12-20 19:11:19 +0800

    feat(Events): show up to 3 featured events

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    import { Events } from "$lib/models/event";
 	import FeaturedEventCard from "$lib/components/events/FeaturedEventCard.svelte";
-	/** @type {import('./$types').PageData} */
 
+	/** @type {import('./$types').PageData} */
 	export let data;
 
 	$: ({ global, events } = data);
+
+	let featured: Events = [];
+    $: featured = events?.slice(0, 3);
 </script>
 
 <div class="container h-full mx-auto flex-col justify-center items-center my-8">
@@ -18,7 +22,7 @@
 			<a href="/events">View all &#8594;</a>
 		</div>
 		<div class="flex my-12 space-x-8">
-			{#each events as event}
+			{#each featured as event}
 				<FeaturedEventCard {event} />
 			{/each}
 		</div>
