5d206ab6ba0f4e73571c7e5c2667bbd775555578
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-26 10:02:50 +0800

    feat(tests): add expected site title

--- website-frontend/tests/layout.spec.ts
+++ website-frontend/tests/layout.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
+const EXPECTED_SITE_TITLE = 'UP Diliman - Department of Computer Science';
+
 test.describe('2-4: Navigation Bar Links to Home', () => {
 	test('Check for home button in navbar', async ({ page }) => {
 		await page.goto('/');
@@ -7,7 +9,7 @@ test.describe('2-4: Navigation Bar Links to Home', () => {
 		const home = await nav.getByRole('link', { name: 'Home' });
 		await home.click();
 		await page.waitForTimeout(5000);
-		expect(await page.title()).toBe('UPD DCS');
+		expect(await page.title()).toBe(EXPECTED_SITE_TITLE);
 	});
 });
 
