f347e8713875f76760e4c7255f8d8f0e15ad4347
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-03 14:32:37 +0800

    feat: style people subpages

--- website-frontend/src/routes/people/[slug]/+page.svelte
+++ website-frontend/src/routes/people/[slug]/+page.svelte
@@ -18,13 +18,13 @@
 	{#if category}
 		<div class="relative">
 			<div
-				class="h-[55vh] bg-cover bg-center md:h-[65vh]"
+				class="h-[45vh] bg-cover bg-center md:h-[60vh]"
 				style="background-image: linear-gradient(to top, #004420, transparent), url('{category.image}')"
 			></div>
 
-			<div class="absolute bottom-9 px-4 md:max-w-[60vw] md:px-32">
-				<h1 class="text-3xl font-bold md:text-5xl">{deslugify_title}</h1>
-				<h4>{@html category.flexible_content}</h4>
+			<div class="absolute bottom-9 px-4 md:max-w-[60vw] md:px-32 text-white">
+				<h1 class="text-3xl font-bold md:text-4xl md:mb-4">{deslugify_title}</h1>
+				<h4 class="hidden text-slate-300 md:block">{@html category.flexible_content}</h4>
 			</div>
 		</div>
 
@@ -33,22 +33,31 @@
 		>
 			{#each people.slice(0, shown) as person}
 				<a href="/people/{category.title}/{person.id}" class="max-w-xs">
-					<div class="flex h-full flex-col rounded-lg bg-white px-3 py-3 shadow-xl">
-						<div class="photo-wrapper p-2">
-							<img
-								class="mx-auto h-32 w-32 rounded-full"
-								src="{PUBLIC_APIURL}/assets/{person.profile_image}"
-								alt="Profile"
-							/>
-						</div>
-						<div class="p-2">
-							<div class="text-center text-xs font-semibold text-gray-400">
-								<p>{person.position}</p>
+					<div class="flex h-full flex-col rounded-lg bg-white py-3 shadow-xl relative overflow-hidden">
+						<!-- replace by background image -->
+						<div class="inset-0 h-44 rounded-t-lg bg-gray-300"></div>
+
+						<div class="z-10 -mt-20 px-3">
+							<div class="bottom-9 photo-wrapper p-2 mb-2">
+								<img
+								  class="mx-auto h-32 w-32 rounded-full border-4 border-gray-200"
+								  src="{PUBLIC_APIURL}/assets/{person.profile_image}"
+								  alt="Profile"
+								/>
 							</div>
-							<h3 class="py-3 text-center text-xl font-medium text-gray-900">
-								{person.first_name}
-								{person.last_name}
-							</h3>
+							<div class="p-2">
+								<div class="text-center text-xs font-semibold text-gray-400">
+									<p>{person.position}</p>
+								</div>
+								<h3 class="py-3 text-center text-xl font-medium text-gray-900">
+									{person.first_name}
+									{person.last_name}
+								</h3>
+								<div class="text-center text-xs font-semibold text-gray-400 flex items-center justify-center">
+									<img src="/path/to/lab_logo.png" alt="Logo" class="h-4 w-4 mr-2">
+									<p class="truncate w-full">Computer Vision and Machine Intelligence Group</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</a>
