45aff8c4bcf90b93994dd3c11d560ffffca745ee
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-04 22:11:39 +0800

    style(Research): add responsive css to overview page

--- website-frontend/src/routes/research/overview/+page.svelte
+++ website-frontend/src/routes/research/overview/+page.svelte
@@ -12,16 +12,17 @@
     <p>The Department of Computer Science houses nine research laboratories catering to theoretical and applied fields in computer science.</p>
 
     {#each laboratories as laboratory}
-        <div class="my-5 grid grid-cols-6 items-center py-5 border-b">
+        <div class="my-5 md:grid md:grid-cols-6 md:items-center py-5 border-b">
             <div>
                 <img src="{PUBLIC_APIURL}/assets/{laboratory.logo}" alt={laboratory.name} />
             </div>
-            <div class="col-span-5 pl-5">
-                <div class="flex items-center">
+            <div class="md:col-span-5 md:pl-5 md:pt-0 pt-5">
+                <div class="md:flex md:items-center">
                     <h3 class="mr-5">{laboratory.name}</h3>
-                    <a href="/research/labs/{laboratory.slug}">View Lab &#8594;</a>
+                    <a href="/research/labs/{laboratory.slug}" class="hidden md:block">View Lab &#8594;</a>
                 </div>
                 <p class="max-h-12 line-clamp-2 overflow-hidden">{laboratory.description}</p>
+                <a href="/research/labs/{laboratory.slug}" class="md:hidden">View Lab &#8594;</a>
             </div>
         </div>
     {/each}
