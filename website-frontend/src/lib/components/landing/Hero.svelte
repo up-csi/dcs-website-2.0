d8ac5ddee9e4d0fe0bb792729c64069cd4b5887c
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-06 17:50:38 +0800

    feat: add button to hero

--- website-frontend/src/lib/components/landing/Hero.svelte
+++ website-frontend/src/lib/components/landing/Hero.svelte
@@ -2,6 +2,7 @@
 	import * as Carousel from "$lib/components/ui/carousel/index.js";
   	import type { CarouselAPI } from "$lib/components/ui/carousel/context.js";
 	import Autoplay from "embla-carousel-autoplay";
+    import { Button } from "$lib/components/ui/button";
  
   	const plugin = Autoplay({ delay: 4000, stopOnInteraction: true });
 	
@@ -37,9 +38,9 @@
 				/>
 			  	<div class="absolute bottom-28 md:px-20 text-white">
 					<h1 class="text-4xl font-bold mb-5">Some Big Bold Description Text</h1>
-					<p class="text-md font-semibold">Some extra description</p>
+					<p class="text-md font-semibold mb-5">Some extra description</p>
+                    <Button href="/">Button</Button>
 			  	</div>
-			  
 			</Carousel.Item>
 		  {/each}
 		</Carousel.Content>
