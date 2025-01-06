96554b265cad62eee481a7af5c4e9660238d6680
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-06 19:56:06 +0800

    chore: run prettier lint

--- website-frontend/src/lib/components/landing/Hero.svelte
+++ website-frontend/src/lib/components/landing/Hero.svelte
@@ -1,51 +1,51 @@
 <script lang="ts">
-	import * as Carousel from "$lib/components/ui/carousel/index.js";
-  	import type { CarouselAPI } from "$lib/components/ui/carousel/context.js";
-	import Autoplay from "embla-carousel-autoplay";
-    import { Button } from "$lib/components/ui/button";
- 
-  	const plugin = Autoplay({ delay: 4000, stopOnInteraction: true });
-	
-  	let api: CarouselAPI;
-  	let current = 0;
-  	let count = 0;
-	
-  	$: if (api) {
-  	  count = api.scrollSnapList().length;
-  	  current = api.selectedScrollSnap() + 1;
-	
-  	  api.on("select", () => {
-  	    current = api.selectedScrollSnap() + 1;
-  	  });
-  	}
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import type { CarouselAPI } from '$lib/components/ui/carousel/context.js';
+	import Autoplay from 'embla-carousel-autoplay';
+	import { Button } from '$lib/components/ui/button';
+
+	const plugin = Autoplay({ delay: 4000, stopOnInteraction: true });
+
+	let api: CarouselAPI;
+	let current = 0;
+	let count = 0;
+
+	$: if (api) {
+		count = api.scrollSnapList().length;
+		current = api.selectedScrollSnap() + 1;
+
+		api.on('select', () => {
+			current = api.selectedScrollSnap() + 1;
+		});
+	}
 </script>
 
 <div>
 	<Carousel.Root
 		bind:api
 		plugins={[plugin]}
-		class="w-full h-[90vh] relative"
+		class="relative h-[90vh] w-full"
 		on:mousenter={plugin.stop}
 		on:mouseleave={plugin.reset}
-  	>
+	>
 		<Carousel.Content>
-		  {#each Array(6) as _, i (i)}
-			<Carousel.Item class="relative h-full">
-				<img
-					src="source/to/image"
-					alt="Carousel Item"
-					class="w-full h-[90vh] bg-slate-400 relative"
-				/>
-			  	<div class="absolute bottom-28 md:px-20 text-white">
-					<h1 class="text-4xl font-bold mb-5">Some Big Bold Description Text</h1>
-					<p class="text-md font-semibold mb-5">Some extra description</p>
-                    <Button href="/">Button</Button>
-			  	</div>
-			</Carousel.Item>
-		  {/each}
+			{#each Array(6) as _, i (i)}
+				<Carousel.Item class="relative h-full">
+					<img
+						src="source/to/image"
+						alt="Carousel Item"
+						class="relative h-[90vh] w-full bg-slate-400"
+					/>
+					<div class="absolute bottom-28 text-white md:px-20">
+						<h1 class="mb-5 text-4xl font-bold">Some Big Bold Description Text</h1>
+						<p class="text-md mb-5 font-semibold">Some extra description</p>
+						<Button href="/">Button</Button>
+					</div>
+				</Carousel.Item>
+			{/each}
 		</Carousel.Content>
-  	</Carousel.Root>
-	<div class="text-white font-semibold absolute -mt-20 text-sm md:px-20">
+	</Carousel.Root>
+	<div class="absolute -mt-20 text-sm font-semibold text-white md:px-20">
 		Item {current} of {count}
 	</div>
 </div>
--- website-frontend/src/lib/components/nav/Header.svelte
+++ website-frontend/src/lib/components/nav/Header.svelte
@@ -1,36 +1,44 @@
 <script lang="ts">
-    import FacebookIcon from '$lib/assets/FacebookIcon.svelte';
-    import XIcon from '$lib/assets/XIcon.svelte';
+	import FacebookIcon from '$lib/assets/FacebookIcon.svelte';
+	import XIcon from '$lib/assets/XIcon.svelte';
 	export let assets;
 </script>
 
-<div class="bg-slate-50 h-14 md:h-16 items-center py-2">
-    <div class="flex justify-between md:px-9 px-2">
-        <div class="flex items-center my-auto">
-            <img
-                src={assets.favicon}
-                alt="UP"
-                class="max-w-xs rounded-full bg-gray-400 hidden md:block h-12 w-12 mr-1"
-            />
-            <img
-                src={assets.favicon}
-                alt="DCS"
-                class="max-w-xs rounded-full bg-gray-400 h-10 w-10 mr-2 md:h-12 md:w-12 md:mr-3"
-            />
-            <div class="font-semibold text-[#004420] ">
-                <h1 class="text-xs md:text-sm">University of the Philippines Diliman</h1>
-                <h1 class="text-md md:text-lg -mt-1 md:-mt-2">Department of Computer Science</h1>
-            </div>
-        </div>
-        <div class="flex items-center my-auto">
-            <a href="https://web.facebook.com/upddcs?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer" 
-               class="hidden md:block h-7 pb-[2px] mr-3 text-gray-400 hover:text-[#004420] transition-colors duration-300">
-                <FacebookIcon />
-            </a>
-            <a href="https://x.com/upcs" target="_blank" rel="noopener noreferrer" 
-               class="hidden md:block h-5 text-gray-400 hover:text-[#004420] transition-colors duration-300">
-                <XIcon />
-            </a>
-        </div>
-    </div>
+<div class="h-14 items-center bg-slate-50 py-2 md:h-16">
+	<div class="flex justify-between px-2 md:px-9">
+		<div class="my-auto flex items-center">
+			<img
+				src={assets.favicon}
+				alt="UP"
+				class="mr-1 hidden h-12 w-12 max-w-xs rounded-full bg-gray-400 md:block"
+			/>
+			<img
+				src={assets.favicon}
+				alt="DCS"
+				class="mr-2 h-10 w-10 max-w-xs rounded-full bg-gray-400 md:mr-3 md:h-12 md:w-12"
+			/>
+			<div class="font-semibold text-[#004420]">
+				<h1 class="text-xs md:text-sm">University of the Philippines Diliman</h1>
+				<h1 class="text-md -mt-1 md:-mt-2 md:text-lg">Department of Computer Science</h1>
+			</div>
+		</div>
+		<div class="my-auto flex items-center">
+			<a
+				href="https://web.facebook.com/upddcs?_rdc=1&_rdr"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="mr-3 hidden h-7 pb-[2px] text-gray-400 transition-colors duration-300 hover:text-[#004420] md:block"
+			>
+				<FacebookIcon />
+			</a>
+			<a
+				href="https://x.com/upcs"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="hidden h-5 text-gray-400 transition-colors duration-300 hover:text-[#004420] md:block"
+			>
+				<XIcon />
+			</a>
+		</div>
+	</div>
 </div>
