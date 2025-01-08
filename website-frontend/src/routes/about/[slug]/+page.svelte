7fd510834f4187cff8d41662b40a86c03e9a9be0
Angelo Convento <convento.angelo@gmail.com>
2025-01-08 11:25:43 +0800

    feat: add `about-pages` page

---
+++ website-frontend/src/routes/about/[slug]/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	$: ({ page } = data);
+</script>
+
+<body class="bg-gray-100">
+	{#if page}
+		<div class="relative">
+			<div
+				class="h-[40vh] bg-cover bg-center md:h-[50vh]"
+				style="background-image: linear-gradient(to top, #004420, transparent)"
+			></div>
+			<div class="absolute bottom-9">
+				<h1 class="px-4 text-3xl font-bold md:max-w-[60vw] md:px-32 md:text-5xl">
+					{page.title}
+				</h1>
+			</div>
+		</div>
+
+		<div class="px-4 py-10 text-base text-[#01152B] md:px-32">
+			{@html page.flexible_content}
+		</div>
+	{:else}
+		<p>Page not found</p>
+	{/if}
+</body>
