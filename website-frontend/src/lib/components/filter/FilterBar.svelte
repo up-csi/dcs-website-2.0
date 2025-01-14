ea727cdffd6c1a83c9d2064dd289935999e8fba0
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-14 17:47:38 +0800

    feat(Filter): add upcoming/past tab

--- website-frontend/src/lib/components/filter/FilterBar.svelte
+++ website-frontend/src/lib/components/filter/FilterBar.svelte
@@ -3,31 +3,55 @@
 	import { ListFilter } from 'lucide-svelte';
 	import FilterButton from "./FilterButton.svelte";
 	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+	import * as Tabs from "$lib/components/ui/tabs";
 	
-	export let controls: FilterControls;
+	export let controls: FilterControls,
+			   timed: boolean = false;
 </script>
 
-<div
-	class="mx-auto md:flex max-w-[94vw] rounded-lg bg-white p-6
-	shadow-lg
-	md:max-w-[82vw] items-center hidden"
->
-	<div class="text-xs font-semibold uppercase text-gray-500 mr-4">
-		<p>FILTER BY</p>
-	</div>
-	{#if controls}
-		<div class="flex items-center gap-x-2">
-			{#each controls as control}
-				<FilterButton {control} />
-			{/each}
+<div class="mx-auto md:flex max-w-[94vw] rounded-lg bg-white p-6
+		shadow-lg
+		md:max-w-[82vw] items-center justify-between hidden">
+	<div
+		class="flex items-center"
+	>
+		<div class="text-xs font-semibold uppercase text-gray-500 mr-4">
+			<p>FILTER BY</p>
 		</div>
+		{#if controls}
+			<div class="flex items-center gap-x-2">
+				{#each controls as control}
+					<FilterButton {control} />
+				{/each}
+			</div>
+		{/if}
+	</div>
+	{#if timed}
+		<Tabs.Root value="upcoming">
+			<Tabs.List class="rounded-3xl [&>*]:rounded-3xl [&>*]:w-36">
+				<Tabs.Trigger value="upcoming">Upcoming</Tabs.Trigger>
+				<Tabs.Trigger value="past">Past</Tabs.Trigger>
+			</Tabs.List>
+		</Tabs.Root>
 	{/if}
 </div>
 
 <div class="md:hidden flex justify-end">
 	<DropdownMenu.Root>
-		<DropdownMenu.Trigger><ListFilter class="h-8 w-8" /></DropdownMenu.Trigger>
+		<DropdownMenu.Trigger>
+			<div class="rounded-full bg-white p-4 shadow-xl mr-2">
+				<ListFilter class="h-8 w-8" />
+			</div>
+		</DropdownMenu.Trigger>
 		<DropdownMenu.Content>
+			{#if timed}
+				<Tabs.Root value="upcoming">
+					<Tabs.List class="rounded [&>*]:rounded [&>*]:w-28">
+						<Tabs.Trigger value="upcoming">Upcoming</Tabs.Trigger>
+						<Tabs.Trigger value="past">Past</Tabs.Trigger>
+					</Tabs.List>
+				</Tabs.Root>
+			{/if}
 			<DropdownMenu.Group>
 				{#if controls}
 					{#each controls as control}
