80c799f20cf05d1676bbd843e748e0af2bd647e7
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-01-14 20:25:33 +0800

    chore: remove .js extension on imports

--- website-frontend/src/routes/events/+page.server.ts
+++ website-frontend/src/routes/events/+page.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 import getDirectusInstance from '$lib/directus';
-import directusFetch from '$lib/server/fetch.js';
+import directusFetch from '$lib/server/fetch';
 
 export async function load({ fetch }) {
 	const directus = await getDirectusInstance(fetch);
