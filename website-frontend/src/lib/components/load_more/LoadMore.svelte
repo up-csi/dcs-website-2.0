fcd44f8e59c5cfb3bf035ef8ebfdb70ef03fabbe
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-14 15:13:27 +0800

    feat(frontend): make a load more button

---
+++ website-frontend/src/lib/components/load_more/LoadMore.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Plus } from "lucide-svelte";
+    export let inc: number;
+
+    export let shown = inc;
+    function loadMore() {
+        shown += inc;
+    }
+</script>
+
+<button class="flex items-center shadow-xl rounded-3xl py-2 px-6" on:click={loadMore}>
+    <Plus class="h-4 w-4 mr-2" />
+    <span>Load More</span>
+</button>
