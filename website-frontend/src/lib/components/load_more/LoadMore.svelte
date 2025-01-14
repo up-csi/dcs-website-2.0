669bd828ad03072642ffccd6751dd40eb6724283
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-14 15:39:44 +0800

    chore: run linters, formatters, and builders

--- website-frontend/src/lib/components/load_more/LoadMore.svelte
+++ website-frontend/src/lib/components/load_more/LoadMore.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import { Plus } from "lucide-svelte";
-    export let inc: number;
+	import { Plus } from 'lucide-svelte';
+	export let inc: number;
 
-    export let shown = inc;
-    function loadMore() {
-        shown += inc;
-    }
+	export let shown = inc;
+	function loadMore() {
+		shown += inc;
+	}
 </script>
 
-<button class="flex items-center shadow-xl rounded-3xl py-2 px-6" on:click={loadMore}>
-    <Plus class="h-4 w-4 mr-2" />
-    <span>Load More</span>
+<button class="flex items-center rounded-3xl px-6 py-2 shadow-xl" on:click={loadMore}>
+	<Plus class="mr-2 h-4 w-4" />
+	<span>Load More</span>
 </button>
