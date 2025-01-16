74ef476ba6509e23ea901ccad4d43b8826a3fcbb
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-16 15:50:09 +0800

    feat: improve UI of events page

--- website-frontend/src/lib/components/events/FeaturedEventCard.svelte
+++ website-frontend/src/lib/components/events/FeaturedEventCard.svelte
@@ -1,17 +1,50 @@
 <script lang="ts">
+	import { PUBLIC_APIURL } from '$env/static/public';
 	import { Event } from '$lib/models/event';
+	import { Calendar, MapPin } from 'lucide-svelte';
 	export let event: Event;
 </script>
 
 <a href="/events/{event.slug}">
-	<div class="card h-fit w-72 max-w-72 snap-center p-4 md:w-96 md:max-w-96">
-		<div class="mb-4 md:flex md:justify-between">
-			<h3>{event.event_headline}</h3>
-			<h3>{new Date(event.date_created).toLocaleDateString()}</h3>
+	<div class="relative flex h-[25rem] flex-col overflow-hidden rounded-lg bg-white shadow-xl text-gray-800">
+
+		<div class="inset-0 flex-grow h-24 rounded-t-lg bg-gray-300 md:h-44">
+			{#if event.hero_image}
+				<img
+					class="h-full w-full rounded-t-lg object-cover"
+					src="{PUBLIC_APIURL}/assets/{event.hero_image}"
+					alt="Background"
+				/>
+			{/if}
 		</div>
-		<div class="max-h-6 overflow-hidden *:truncate">
-			{@html event.event_content}
+
+		<div>
+			<div class="absolute rounded-full w-6 h-6 bg-slate-100 shadow-inner -mt-2 -left-3"></div>
+			<div class="absolute rounded-full w-6 h-6 bg-slate-100 shadow-inner -mt-2 -right-3"></div>
 		</div>
-		<p>View details &#8594;</p>
+
+		<div class="p-4 space-y-2">
+			<div class="font-bold text-lg">
+				<h1>{event.event_headline}</h1>
+			</div> 
+			<div class="font-medium text-sm space-x-2 flex items-center">
+				<Calendar class="h-4 w-4" />
+				<p>7 - 8 February 2025</p> 
+			</div>
+			{#if event.location}
+				<div class="text-sm text-gray-500 space-x-2 flex items-center">
+					<MapPin class="h-4 w-4" />
+					<p>{event.location}</p> 
+				</div>
+			{/if}
+			{#if event.tags}
+				<div class="font-bold text-xs text-gray-600 py-1">
+					{#each event.tags as tag, index}
+						<p>{tag}{index < (event.tags.length - 1) ? ',' : ''}</p> 
+					{/each}
+				</div>
+			{/if}
+		</div>
+
 	</div>
 </a>
--- website-frontend/src/routes/events/+page.svelte
+++ website-frontend/src/routes/events/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	/** @type {import('./$types').PageData} */
 	import type { FilterControls } from '$lib/types/filter_controls';
+	import Banner from '$lib/components/events/Banner.svelte';
+	import * as Carousel from "$lib/components/ui/carousel/index.js";
 	import FeaturedEventCard from '$lib/components/events/FeaturedEventCard.svelte';
 	import FilterBar from '$lib/components/filter/FilterBar.svelte';
 	import LoadMore from '$lib/components/load_more/LoadMore.svelte';
@@ -20,44 +22,64 @@
 	const timed = true;
 </script>
 
-<div class="container mx-auto my-8 h-full flex-col items-center justify-center">
-	<div class="my-12 flex space-x-8 overflow-x-auto">
-		{#each featured as event}
-			<FeaturedEventCard {event} />
-		{/each}
+<body class="bg-slate-50">
+	<div class="relative z-0">
+		<Banner
+			title="Events"
+			background_image={events.background_image ?? ''}
+			flexible_content={events.flexible_content}
+		/>
 	</div>
-	<div class="flex items-center justify-between md:block">
-		<h1>Find Events</h1>
-		<FilterBar {controls} {timed} />
+
+	<div class="-mt-28 pb-16">
+		<Carousel.Root>
+			<Carousel.Content class="-ml-6 py-10 px-16">
+				{#each featured as event}
+					<Carousel.Item class="basis-1 pl-6 md:basis-1/4">
+						<FeaturedEventCard {event} />
+					</Carousel.Item>
+				{/each}
+			</Carousel.Content>
+		</Carousel.Root>
 	</div>
-	<div class="my-6">
-		{#each eventList as event}
-			<div class="rounded border p-4 md:grid md:grid-cols-6">
-				<div class="md:col-span-5">
-					<h3>{event.event_headline}</h3>
-					<h3 class="block md:hidden">{new Date(event.date_created).toLocaleDateString()}</h3>
-					<div class="max-h-6 overflow-hidden *:truncate">
-						{@html event.event_content}
-					</div>
-					<p>View details &#8594;</p>
-					<div class="flex items-center">
-						<p>Tags:</p>
-						{#if event.tags}
-							{#each event.tags as tag}
-								<p class="mx-2 rounded border px-2 py-1">{tag}</p>
-							{/each}
-						{/if}
-					</div>
-				</div>
-				<div class="hidden place-content-center md:block">
-					<h3>{new Date(event.date_created).toLocaleDateString()}</h3>
-				</div>
-			</div>
-		{/each}
-	</div>
-	{#if shown < events.length}
-		<div class="flex items-center justify-center">
-			<LoadMore {inc} bind:shown />
+
+	<h1 class="mb-8 px-4 text-3xl font-bold text-gray-900 md:px-16">Related Events</h1>
+
+
+	<div class="container mx-auto my-8 h-full flex-col items-center justify-center">
+
+		<div class="flex items-center justify-between md:block">
+			<FilterBar {controls} {timed} />
 		</div>
-	{/if}
-</div>
+		<div class="my-6 text-gray-800">
+			{#each eventList as event}
+				<div class="rounded border p-4 md:grid md:grid-cols-6">
+					<div class="md:col-span-5">
+						<h3>{event.event_headline}</h3>
+						<h3 class="block md:hidden">{new Date(event.date_created).toLocaleDateString()}</h3>
+						<div class="max-h-6 overflow-hidden *:truncate">
+							{@html event.event_content}
+						</div>
+						<p>View details &#8594;</p>
+						<div class="flex items-center">
+							<p>Tags:</p>
+							{#if event.tags}
+								{#each event.tags as tag}
+									<p class="mx-2 rounded border px-2 py-1">{tag}</p>
+								{/each}
+							{/if}
+						</div>
+					</div>
+					<div class="hidden place-content-center md:block">
+						<h3>{new Date(event.date_created).toLocaleDateString()}</h3>
+					</div>
+				</div>
+			{/each}
+		</div>
+		{#if shown < events.length}
+			<div class="flex items-center justify-center">
+				<LoadMore {inc} bind:shown />
+			</div>
+		{/if}
+	</div>
+</body>
