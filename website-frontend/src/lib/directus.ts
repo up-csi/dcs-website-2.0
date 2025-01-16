fc9272dcf9963ec37aa4004a3f35fe72acbf75da
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-01-16 10:28:13 +0800

    refactor!: relocate fetch.ts to types

--- website-frontend/src/lib/directus.ts
+++ website-frontend/src/lib/directus.ts
@@ -2,7 +2,7 @@ import { createDirectus, rest, authentication } from '@directus/sdk';
 import { EMAIL, PASSWORD } from '$env/static/private';
 import { PUBLIC_APIURL } from '$env/static/public';
 import { type Schema } from './models/schema';
-import type { Fetch } from './fetch';
+import type { Fetch } from './types/fetch';
 
 async function getDirectusInstance(fetch: Fetch | null) {
 	const options = fetch ? { globals: { fetch } } : {};
