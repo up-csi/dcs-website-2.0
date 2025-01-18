55ea0eed4aa5a60286ca80619d1e2ee16d956acd
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-01-18 09:26:45 +0800

    feat: add time filter logic to events

--- website-frontend/src/routes/events/+page.server.ts
+++ website-frontend/src/routes/events/+page.server.ts
@@ -1,12 +1,68 @@
 /** @type {import('./$types').PageServerLoad} */
 import getDirectusInstance from '$lib/directus';
-import obtainSchema from '$lib/server/schema';
+import { readItems } from '@directus/sdk';
 
-export async function load({ fetch }) {
+export async function load({ fetch, url }) {
 	const directus = getDirectusInstance(fetch);
+	const filters = {
+		location: url.searchParams.get('location'),
+		discipline: url.searchParams.get('discipline'),
+		time: url.searchParams.get('time')
+	};
+	const events = await (async () => {
+		if (filters.time === 'upcoming') {
+			return await directus.request(
+				readItems('events', {
+					filter: {
+						_and: [
+							{ location: { _eq: filters.location } },
+							{
+								start_date: {
+									_gte: '$NOW'
+								}
+							}
+						]
+					}
+				})
+			);
+		}
+		if (filters.time === 'past') {
+			return await directus.request(
+				readItems('events', {
+					filter: {
+						_and: [
+							{ location: { _eq: filters.location } },
+							{
+								_or: [
+									{
+										_and: [
+											{
+												end_date: {
+													_null: true
+												}
+											},
+											{
+												start_date: {
+													_lte: '$NOW'
+												}
+											}
+										]
+									},
+									{
+										end_date: {
+											_nnull: true,
+											_lte: '$NOW'
+										}
+									}
+								]
+							}
+						]
+					}
+				})
+			);
+		}
+		return await directus.request(readItems('events'));
+	})();
 
-	const keys = ['events'];
-	const schema = await obtainSchema(directus, keys);
-
-	return { schema };
+	return { events };
 }
