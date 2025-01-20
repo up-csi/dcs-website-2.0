7d0490d47ea0ca0375303b966d34a0f97ca00029
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-01-20 13:26:38 +0800

    feat: add type guard for type 'datetime' with '$NOW' fallback

--- website-frontend/src/lib/models-helpers.ts
+++ website-frontend/src/lib/models-helpers.ts
@@ -3,5 +3,8 @@ import { transform } from 'valibot';
 
 export const cleanHtml = transform((input: string) => sanitize(input));
 export const toDateTime = transform((input: string) => {
-	return input as 'datetime';
+	if (!isNaN(Date.parse(input))) {
+		return input as 'datetime';
+	}
+	return Date.now().toString() as 'datetime';
 });
