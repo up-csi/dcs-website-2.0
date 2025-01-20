23d602110c9b67ca2729e0467b819cc1dc10bb0d
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-01-20 13:19:59 +0800

    fix: set type of event dates to 'datetime'

--- website-frontend/src/lib/models-helpers.ts
+++ website-frontend/src/lib/models-helpers.ts
@@ -2,3 +2,6 @@ import sanitize from 'sanitize-html';
 import { transform } from 'valibot';
 
 export const cleanHtml = transform((input: string) => sanitize(input));
+export const toDateTime = transform((input: string) => {
+	return input as 'datetime';
+});
