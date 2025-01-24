eb673b08d054fd02fe839b256fe16f2e2b446a03
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-24 16:43:08 +0800

    feat: add route for lab pages

---
+++ website-frontend/src/routes/research/labs/[slug]/+page.server.ts
@@ -0,0 +1,28 @@
+/** @type {import('./$types').PageServerLoad} */
+import { readItems } from '@directus/sdk';
+import { parse } from 'valibot';
+import { Laboratory } from '$lib/models/laboratories';
+import getDirectusInstance from '$lib/directus';
+import { error } from '@sveltejs/kit';
+
+export async function load({ params, fetch }) {
+	const directus = getDirectusInstance(fetch);
+	const slug = params.slug;
+
+	const laboratories = await directus.request(
+		readItems('laboratories', {
+			filter: {
+				slug: { _eq: slug }
+			},
+			limit: 1
+		})
+	);
+
+	if (!laboratories || laboratories.length === 0) {
+		throw error(404, 'Lab not found');
+	}
+
+	return {
+		laboratory: parse(Laboratory, laboratories[0]),
+	};
+}
---
+++ website-frontend/src/routes/research/labs/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	import { PUBLIC_APIURL } from '$env/static/public';
+	export let data;
+	const { laboratory } = data;
+</script>
+
+<div>
+	<h1 class="py-4 text-3xl font-bold">{laboratory.name} {laboratory.slug} {laboratory.id}</h1>
+	<img
+		src="{PUBLIC_APIURL}/assets/{laboratory.logo}"
+		alt="{laboratory.slug} logo"
+	/>
+	<p>{laboratory.description}</p>
+	<p>{laboratory.location}</p>
+	<p>{laboratory.contact_email}</p>
+</div>
