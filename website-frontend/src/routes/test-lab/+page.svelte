5ca769a8bdfbf5670e61bb917b82da0b2ebe5097
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-27 16:59:34 +0800

    chore: add description in test-lab

--- website-frontend/src/routes/test-lab/+page.svelte
+++ website-frontend/src/routes/test-lab/+page.svelte
@@ -6,13 +6,15 @@
 
 <Hero />
 
-<div>
-	<h1 class="py-4 text-3xl font-bold">name slug ID</h1>
-	<img
-		src="{PUBLIC_APIURL}/assets/lab_logo"
-		alt="slug logo"
-	/>
-	<p>description</p>
-	<p>location</p>
-	<p>contact_email</p>
+<div class="px-3">
+
+	<div class="p-5 rounded-lg bg-slate-400">
+		<p>location</p>
+		<p>contact_email</p>
+		<p>established</p>
+	</div>
+
+	<div class="font-semibold"> 
+		<p>Lorem ipsum dalor my anoconda don't my anaconda don't my anconda don't want nothing unless you got buns hun. Oh my gosh. Look at that dog. One thing about me: I'm the baddest alive.</p>
+	</div>
 </div>
