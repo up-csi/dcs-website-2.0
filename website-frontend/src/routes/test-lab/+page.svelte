e21848b6b17811dd2765b15a7f5cc91476072c5e
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-01-31 14:07:31 +0800

    chore: adjust lab-org mobile styling

--- website-frontend/src/routes/test-lab/+page.svelte
+++ website-frontend/src/routes/test-lab/+page.svelte
@@ -7,16 +7,17 @@
 <Hero title="UP Center for Student Innovations" background_image="" />
 
 <div class="bg-primary">
-	<div class="px-3 py-4 md:pl-[260px] lg:pl-[302px] md:max-w-6xl md:space-y-12 md:py-20">
 
-		<div class="px-2 md:px-0 text-primary-foreground font-semibold text-xl">
+	<div class="px-3 py-4 md:pl-[260px] lg:pl-[302px] md:max-w-6xl space-y-7 md:space-y-12 md:py-20">
+
+		<div class="px-2 md:px-0 text-primary-foreground font-semibold text-xl leading-snug md:leading-normal">
 			<p>
 				Lorem ipsum dalor my anoconda don't my anaconda don't my anconda don't want nothing unless you
 				got buns hun. Oh my gosh. Look at that dog. One thing about me: I'm the baddest alive.
 			</p>
 		</div>
 
-		<div class="rounded-lg bg-background/20 p-6 space-y-5">
+		<div class="rounded-lg bg-background/20 space-y-5 p-6 md:p-10">
 			<div> 
 				<p class="text-primary-foreground/60">Location</p>
 				<p class="text-primary-foreground font-semibold text-lg -mt-1">Melchor Hall 4th Floor</p>
