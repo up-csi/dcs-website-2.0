c478125edfb2fda32a9dd03a55de31071f886fd3
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-01-31 23:30:37 +0800

    feat: add featured outputs page

---
+++ website-frontend/src/routes/research/outputs/+page.server.ts
@@ -0,0 +1,17 @@
+const dummy = [
+    {
+        title: 'Lusog: Development of a Web Application for Detecting Malnutrition in Filipino Children Aged 0–19',
+        date_published: '2023-05-31T18:30:50.000Z',
+        authors: [
+            'Eriene D. Galinato',
+            'Julia Mari M. Martinez',
+            'Ma. Vanessa Clarisse C. Ponteres'
+        ]
+    }
+];
+
+export async function load() {
+	return {
+		publications: dummy
+	};
+}
---
+++ website-frontend/src/routes/research/outputs/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+
+    import Banner from '$lib/components/banner/Banner.svelte';
+    import CardPanel from '$lib/components/panel/CardPanel.svelte';
+    import FilterBar from '$lib/components/filter/FilterBar.svelte';
+    import type { FilterControls } from '$lib/types/filter_controls.js';
+    import PublicationCard from '$lib/components/card/PublicationCard.svelte';
+
+    export let data;
+
+    $: ({ publications } = data);
+
+    let controls: FilterControls = []
+</script>
+
+<Banner title="Featured Outputs" />
+
+<div class="my-12">
+    <FilterBar {controls} />
+</div>
+
+<CardPanel>
+    {#each publications as publication}
+        <PublicationCard {publication} />
+    {/each}
+</CardPanel>
