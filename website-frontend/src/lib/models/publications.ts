a45aabe90f0ce2d332c3002b3fe4011b94bde179
Angelo Convento <convento.angelo@gmail.com>
2025-02-03 16:49:07 +0800

    feat: add `Publication` model

---
+++ website-frontend/src/lib/models/publications.ts
@@ -0,0 +1,37 @@
+import { array, isoDate, nullable, object, pipe, string, type InferOutput } from 'valibot';
+
+const Author = object({
+	first_name: string(),
+	last_name: string()
+});
+
+const AccessLink = object({
+	url: string()
+});
+
+export const Publication = object({
+	id: string(),
+	title: string(),
+	publish_date: nullable(pipe(string(), isoDate())),
+	authors: array(Author),
+	abstract: string(),
+	laboratory: object({
+		id: string(),
+		name: string()
+	}),
+	hero_image: string(),
+	publication_tag: nullable(
+		object({
+			id: string(),
+			name: string()
+		})
+	),
+	access_links: nullable(array(AccessLink))
+});
+
+export const Publications = array(Publication);
+
+export type Author = InferOutput<typeof Author>;
+export type AccessLink = InferOutput<typeof AccessLink>;
+export type Publication = InferOutput<typeof Publication>;
+export type Publications = InferOutput<typeof Publications>;
