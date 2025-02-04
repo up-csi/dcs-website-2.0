a78c2c493fa3da36774d374b89e45435c6a732d4
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-02-05 02:05:04 +0800

    feat: parameterize lab hero

--- website-frontend/src/lib/components/carousels/LabHero.svelte
+++ website-frontend/src/lib/components/carousels/LabHero.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { deslugify } from '$lib/utils';
 	import { PUBLIC_APIURL } from '$env/static/public';
+	import { Image } from 'lucide-svelte';
 
 	import * as Carousel from '$lib/@shadcn-svelte/ui/carousel/index';
 	import type { CarouselAPI } from '$lib/@shadcn-svelte/ui/carousel/context';
@@ -8,13 +9,13 @@
 	import Autoplay from 'embla-carousel-autoplay';
 	import Fade from 'embla-carousel-fade';
 
+	export let background_images: string[] = [];
+	export let logo_image: string = '';
+	export let link: string = '';
 	export let title: string;
-	export let background_image: string;
-
-	$: deslugify_title = deslugify(title);
 
 	const autoplayPlugin = Autoplay({ delay: 8000, stopOnInteraction: true });
-	const fadePlugin = Fade({ duration: 3000 });
+	const fadePlugin = Fade();
 
 	let api: CarouselAPI;
 	let current = 0;
@@ -29,7 +30,16 @@
 		});
 	}
 
-	const lab_logo = 'x';
+	$: if (background_images.length === 0) {
+		background_images = [
+			`default1.jpg`,
+			`default2.jpg`,
+			`default3.jpg`,
+			`default4.jpg`,
+		];
+	}
+
+	$: deslugify_title = deslugify(title);
 </script>
 
 <div>
@@ -37,13 +47,13 @@
 		<Carousel.Root
 			bind:api
 			plugins={[autoplayPlugin, fadePlugin]}
-			class="h-[82vh] w-full md:h-[75vh]"
+			class="h-[82vh] w-full md:h-[80vh]"
 		>
 			<Carousel.Content>
-				{#each Array(6) as _}
-					<Carousel.Item class="relative h-full">
+				{#each background_images as background_image}
+					<Carousel.Item class="h-full">
 						<div
-							class="h-[82vh] bg-cover bg-center md:h-[75vh]"
+							class="h-[82vh] bg-cover bg-center md:h-[80vh]"
 							style="background-image: linear-gradient(to top, hsl(var(--primary)), transparent), url('{PUBLIC_APIURL}/assets/{background_image}')"
 						></div>
 					</Carousel.Item>
@@ -53,25 +63,25 @@
 	</div>
 
 	<div
-		class="absolute bottom-36 flex w-full flex-col items-center text-secondary-foreground md:flex-row md:pl-5 md:pr-2 lg:px-16"
+		class="absolute bottom-40 flex w-full flex-col items-center text-secondary-foreground md:bottom-28 md:flex-row md:pl-5 md:pr-2 lg:px-16 lg:bottom-32"
 	>
 		<div
-			class="mx-auto flex h-28 w-28 flex-shrink-0 items-center justify-center rounded-full border-4 border-gray-200 bg-gray-100 md:mr-20 md:h-40 md:w-40"
+			class="mx-auto flex h-28 w-28 flex-shrink-0 items-center justify-center rounded-full border-4 border-gray-200 bg-gray-100 md:h-40 md:w-40 md:mr-10 lg:mr-20"
 		>
-			{#if lab_logo}
+			{#if logo_image}
 				<img
 					class="h-full w-full rounded-full object-cover"
-					src="{PUBLIC_APIURL}/assets/{lab_logo}"
+					src="{PUBLIC_APIURL}/assets/{logo_image}"
 					alt="Logo"
 				/>
 			{:else}
-				<h1 class="text-center text-2xl font-medium text-[#004420a2]">`⎚⩊⎚´</h1>
+				<Image class="text-secondary h-10 w-10" />
 			{/if}
 		</div>
 
 		<div class="mt-4 w-full md:mt-0">
 			<div class="flex w-full justify-center md:justify-start">
-				<h1 class="max-w-lg text-center text-4xl font-bold md:text-start md:text-5xl">
+				<h1 class="max-w-2xl text-center text-4xl font-bold md:leading-[3.2rem] md:text-start md:text-5xl">
 					{deslugify_title}
 				</h1>
 			</div>
@@ -82,15 +92,24 @@
 				<div class="my-5 flex gap-2 md:mb-0 md:mt-5">
 					{#each Array(count) as _, index}
 						<button
-							class="h-2 w-2 rounded-full transition-colors duration-300"
-							class:bg-primary-foreground={index === current - 1}
-							class:bg-gray-400={index !== current - 1}
+							class="h-2 w-2 rounded-full transition-all duration-400
+							{index === current - 1 ? 'bg-primary-foreground' 
+							: 'bg-gray-400'}"
 							on:click={() => api.scrollTo(index)}
 						></button>
 					{/each}
 				</div>
 
-				<Button href="/" class="max-w-xs rounded-full bg-background/20">Visit Our Site</Button>
+				{#if link}
+					<Button 
+						href="{link}" 
+						target="_blank" 
+						rel="noopener noreferrer" 
+						class="max-w-xs rounded-full bg-background/20 hover:bg-background/30"
+					>
+						Visit Our Site
+					</Button>
+				{/if}
 			</div>
 		</div>
 	</div>
--- website-frontend/src/routes/test-lab/+page.svelte
+++ website-frontend/src/routes/test-lab/+page.svelte
@@ -1,19 +1,25 @@
 <script>
 	/** @type {import('./$types').PageData} */
-	import { PUBLIC_APIURL } from '$env/static/public';
 	import Hero from '$lib/components/carousels/LabHero.svelte';
+
 </script>
 
-<Hero title="UP Center for Student Innovations" background_image="" />
+
+
+<Hero 
+	title="Computer Vision & Machine Intelligence Laboratory" 
+	background_images={[]}
+	logo_image=""
+	link="https://up-csi.org/"
+/>
 
 <div class="bg-primary">
-	<div class="space-y-7 px-3 py-4 md:max-w-6xl md:space-y-12 md:py-20 md:pl-[260px] lg:pl-[302px]">
+	<div class="px-4 space-y-9 py-4 md:max-w-6xl md:space-y-12 md:py-12 md:px-16 lg:pl-[302px]">
 		<div
-			class="px-2 text-xl font-semibold leading-snug text-primary-foreground md:px-0 md:leading-normal"
+			class="text-2xl font-semibold text-primary-foreground leading-tight md:leading-snug"
 		>
 			<p>
-				Lorem ipsum dalor my anoconda don't my anaconda don't my anconda don't want nothing unless
-				you got buns hun. Oh my gosh. Look at that dog. One thing about me: I'm the baddest alive.
+				The premiere software development org in UP Diliman. The number one DCS org in the university of the philippines
 			</p>
 		</div>
 
@@ -34,10 +40,9 @@
 			</div>
 		</div>
 
-		<div class="px-2 text-primary-foreground md:px-0">
+		<div class="text-lg leading-normal text-primary-foreground">
 			<p>
-				Lorem ipsum dalor my anoconda don't my anaconda don't my anconda don't want nothing unless
-				you got buns hun. Oh my gosh. Look at that dog. One thing about me: I'm the baddest alive.
+				Street skater Margielyn Didal started out skating when she was just 12 at the late Concave Park in the Philippine city of Cebu. When that refuge closed, she was left to hone her skate craft in the urban jungle and was discovered by her coach Daniel Bautista, who recognised her talent and started training her for local competitions.
 			</p>
 		</div>
 	</div>
