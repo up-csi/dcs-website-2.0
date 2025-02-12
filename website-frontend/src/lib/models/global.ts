5f173714404175a505a30154561e935fec702683
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-02-12 10:57:56 +0800

    feat: extend Global model for initial header and footer inputs

--- website-frontend/src/lib/models/global.ts
+++ website-frontend/src/lib/models/global.ts
@@ -1,9 +1,30 @@
-import { object, string, type InferOutput } from 'valibot';
+import { array, intersect, object, string, type InferOutput } from 'valibot';
 
-export const Global = object({
+const Metadata = object({
 	title: string(),
 	description: string(),
 	favicon: string()
 });
 
+const Header = object({});
+
+const Footer = object({
+	address: array(
+		object({
+			address_line: string()
+		})
+	),
+	contact_number: string(),
+	contact_email: string(),
+	quick_links: array(
+		object({
+			name: string(),
+			link: string()
+		})
+	),
+	facebook_link: string(),
+	x_link: string()
+});
+
+export const Global = intersect([Metadata, Header, Footer]);
 export type Global = InferOutput<typeof Global>;
