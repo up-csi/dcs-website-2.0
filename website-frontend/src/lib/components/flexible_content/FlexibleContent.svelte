d3581b296bdfba4823cdc875afebe33791c454e3
Angelo Convento <convento.angelo@gmail.com>
2025-02-17 12:25:46 +0800

    feat: enhance WYSIWYG content handling with dark mode support

--- website-frontend/src/lib/components/flexible_content/FlexibleContent.svelte
+++ website-frontend/src/lib/components/flexible_content/FlexibleContent.svelte
@@ -2,15 +2,16 @@
 	import { onMount } from 'svelte';
 	import { enhanceWysiwygContent } from '.';
 	export let content: string = '';
+	export let isDark: boolean = false;
 
 	let enhancedContent = content;
 
 	onMount(() => {
-		enhancedContent = content ? enhanceWysiwygContent(content) : '';
+		enhancedContent = content ? enhanceWysiwygContent(content, isDark) : '';
 	});
 
 	$: if (typeof window !== 'undefined') {
-		enhancedContent = content ? enhanceWysiwygContent(content) : '';
+		enhancedContent = content ? enhanceWysiwygContent(content, isDark) : '';
 	}
 </script>
 
