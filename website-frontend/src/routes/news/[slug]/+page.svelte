c2f226ce4f0522bdefc2bf227333a0f9cc97a06b
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-02-20 12:58:17 +0800

    feat: enhance news details banner and content

--- website-frontend/src/routes/news/[slug]/+page.svelte
+++ website-frontend/src/routes/news/[slug]/+page.svelte
@@ -9,15 +9,14 @@
 
 <body>
 	{#if news_item}
+		<!-- TODO: Create a dedicated news banner and frontend page formatting -->
 		<Banner
 			title={news_item.title}
-			flexible_content={news_item.summary}
+			flexible_content="{news_item.summary} <br /><br /> Written by {news_item.user_created
+				.first_name}
+			{news_item.user_created.last_name} | {news_item.date_created}"
 			from_bottom={'bottom-40'}
 		/>
-		<p>
-			Written by {news_item.user_created.first_name}
-			{news_item.user_created.last_name} | {news_item.date_created}
-		</p>
 		<br />
 
 		<div class="px-4 py-10 text-base md:px-32">
