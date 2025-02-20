839f699eb7198888bcf3391b25e5b62fa51e4643
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-02-20 12:38:38 +0800

    feat: add news details page at `/news`

---
+++ website-frontend/src/routes/news/[slug]/+page.server.ts
@@ -0,0 +1,33 @@
+/** @type {import('./$types').PageServerLoad} */
+import { readItems } from '@directus/sdk';
+import getDirectusInstance from '$lib/directus';
+import { error } from '@sveltejs/kit';
+
+export async function load({ params, fetch }) {
+	const directus = getDirectusInstance(fetch);
+	const news = await directus.request(
+		readItems('news', {
+			fields: [
+				'*',
+				'user_created.first_name',
+				'user_created.last_name',
+				'user_updated.first_name',
+				'user_updated.last_name'
+			],
+			filter: {
+				slug: {
+					_eq: params.slug
+				}
+			},
+			limit: 1
+		})
+	);
+
+	if (!news.length) {
+		throw error(404, 'News item not found');
+	}
+
+	const news_item = news[0];
+
+	return { news_item };
+}
---
+++ website-frontend/src/routes/news/[slug]/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	import Banner from '$lib/components/banner/Banner.svelte';
+	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
+	export let data;
+
+	$: ({ news_item } = data);
+</script>
+
+<body>
+	{#if news_item}
+		<Banner
+			title={news_item.title}
+			flexible_content={news_item.summary}
+			from_bottom={'bottom-40'}
+		/>
+		<p>
+			Written by {news_item.user_created.first_name}
+			{news_item.user_created.last_name} | {news_item.date_created}
+		</p>
+		<br />
+
+		<div class="px-4 py-10 text-base md:px-32">
+			{#if news_item.flexible_content}
+				<FlexibleContent content={news_item.flexible_content} />
+			{:else}
+				<p>Page is empty.</p>
+			{/if}
+		</div>
+	{:else}
+		<p>Page not found</p>
+	{/if}
+</body>
