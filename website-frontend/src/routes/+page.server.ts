e80a6b87b1b9a6c41f27e9a4fde12849af4ee600
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-02-20 12:38:20 +0800

    feat: add barebones news display at home page

--- website-frontend/src/routes/+page.server.ts
+++ website-frontend/src/routes/+page.server.ts
@@ -5,9 +5,21 @@ import getDirectusInstance from '$lib/directus';
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	const global = await directus.request(readSingleton('global'));
+	const news = await directus.request(
+		readItems('news', {
+			fields: [
+				'*',
+				'user_created.first_name',
+				'user_created.last_name',
+				'user_updated.first_name',
+				'user_updated.last_name'
+			],
+			sort: ['-date_created']
+		})
+	);
 	const events = await directus.request(
 		readItems('events', { fields: ['*', 'event_area.name', 'event_tags.events_tags_id.name'] })
 	);
 
-	return { global, events };
+	return { global, news, events };
 }
