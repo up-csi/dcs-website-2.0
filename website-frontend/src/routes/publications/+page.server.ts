a6862b5577d6470ca0ff0f789a2fc9b2a7abf0ae
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-02-26 13:32:28 +0800

    feat: add laboratory filter logic to publications

--- website-frontend/src/routes/publications/+page.server.ts
+++ website-frontend/src/routes/publications/+page.server.ts
@@ -2,12 +2,34 @@
 import { readItems } from '@directus/sdk';
 import getDirectusInstance from '$lib/directus';
 
-export async function load({ fetch }) {
+export async function load({ fetch, url }) {
 	const directus = getDirectusInstance(fetch);
+
+	const filters = {
+		year: url.searchParams.getAll('year'),
+		laboratories: url.searchParams.getAll('laboratory'),
+		tags: url.searchParams.getAll('laboratory')
+	};
+
+	const laboratories_filters = await directus
+		.request(
+			readItems('laboratories', {
+				fields: ['name'],
+				sort: ['name']
+			})
+		)
+		.then((res) => res.map(({ name }) => name));
+
 	const publications = await directus.request(
 		readItems('publications', {
-			sort: '-publish_date'
+			filter: {
+				laboratory: {
+					name: { _in: filters.laboratories.length !== 0 ? filters.laboratories : undefined }
+				}
+			},
+			sort: ['-publish_date']
 		})
 	);
-	return { publications };
+
+	return { publications, laboratories_filters };
 }
