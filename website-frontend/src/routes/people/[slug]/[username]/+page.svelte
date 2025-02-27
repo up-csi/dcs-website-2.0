d24ea42d0d59666e7e87d272ba3706dd56dc57a7
Angelo Convento <convento.angelo@gmail.com>
2025-02-27 13:03:40 +0800

    fix: only show Publications header if person has publication/s

--- website-frontend/src/routes/people/[slug]/[username]/+page.svelte
+++ website-frontend/src/routes/people/[slug]/[username]/+page.svelte
@@ -78,7 +78,7 @@
 	<div class="-mt-[1px] bg-[#343541]">
 		<div
 			class="space-y-9 px-4 pb-16 pt-9 md:max-w-6xl md:space-y-12 md:px-10 md:pb-24 md:pt-12 lg:pl-[369px]"
-		>	
+		>
 			<div class="text-lg leading-normal text-primary-foreground">
 				{#if person.educational_attainment}
 					<ul class="list-disc pl-5 text-primary-foreground">
@@ -103,57 +103,59 @@
 				<InfoCard office={person.location ?? ''} interests={person.interests ?? ''} />
 			{/if}
 
-			<h2 class="text-2xl font-bold text-primary-foreground">Publications</h2>
-			{#each publications as publication, i}
-				<div class="space-y-4">
+			{#if publications.length > 0}
+				<h2 class="text-2xl font-bold text-primary-foreground">Publications</h2>
+				{#each publications as publication, i}
 					<div class="space-y-4">
-						<div class="flex flex-col">
-							<div class="flex items-center space-x-4">
-								{#if publication.hero_image}
-									<img
-										src="{PUBLIC_APIURL}/assets/{publication.hero_image}"
-										alt={publication.title}
-										class="h-16 w-16 rounded-md"
-									/>
-								{:else}
-									<div class="h-16 w-16 rounded-md bg-gray-300"></div>
-								{/if}
-								<div class="flex flex-col space-y-1">
-									<p class="text-lg font-bold text-primary-foreground">{publication.title}</p>
-									<p class="text-sm text-gray-400">{publication.publish_date}</p>
+						<div class="space-y-4">
+							<div class="flex flex-col">
+								<div class="flex items-center space-x-4">
+									{#if publication.hero_image}
+										<img
+											src="{PUBLIC_APIURL}/assets/{publication.hero_image}"
+											alt={publication.title}
+											class="h-16 w-16 rounded-md"
+										/>
+									{:else}
+										<div class="h-16 w-16 rounded-md bg-gray-300"></div>
+									{/if}
+									<div class="flex flex-col space-y-1">
+										<p class="text-lg font-bold text-primary-foreground">{publication.title}</p>
+										<p class="text-sm text-gray-400">{publication.publish_date}</p>
+									</div>
 								</div>
-							</div>
-							<div class="ml-20 flex flex-col">
-								{#if authors[i]}
-									<p class="text-md text-primary-foreground">Authors:</p>
-									<div class="flex flex-col">
-										{#each authors[i] as author}
-											<p class="text-sm text-gray-400">
-												{author.first_name}
-												{author.last_name}
-											</p>
-										{/each}
-									</div>
-								{/if}
-								{#if linked_authors[i]}
-									<div class="flex flex-col">
-										{#each linked_authors[i] as linked_author}
-											<a
-												class="text-sm text-blue-400"
-												href="/people/{linked_author.category}/{linked_author.username}"
-												data-sveltekit-reload
-											>
-												{linked_author.first_name}
-												{linked_author.last_name}
-											</a>
-										{/each}
-									</div>
-								{/if}
+								<div class="ml-20 flex flex-col">
+									{#if authors[i]}
+										<p class="text-md text-primary-foreground">Authors:</p>
+										<div class="flex flex-col">
+											{#each authors[i] as author}
+												<p class="text-sm text-gray-400">
+													{author.first_name}
+													{author.last_name}
+												</p>
+											{/each}
+										</div>
+									{/if}
+									{#if linked_authors[i]}
+										<div class="flex flex-col">
+											{#each linked_authors[i] as linked_author}
+												<a
+													class="text-sm text-blue-400"
+													href="/people/{linked_author.category}/{linked_author.username}"
+													data-sveltekit-reload
+												>
+													{linked_author.first_name}
+													{linked_author.last_name}
+												</a>
+											{/each}
+										</div>
+									{/if}
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			{/each}
+				{/each}
+			{/if}
 		</div>
 	</div>
 </div>
