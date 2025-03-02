18c35c27193335339268dc584577da785ad882e7
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-02 23:50:00 +0800

    feat: add more news component with link

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -17,7 +17,7 @@
 <div class="container mx-auto my-8 flex h-full flex-col justify-center gap-y-5">
 	<div class="flex justify-between">
 		<h2 class="text-xl font-bold md:text-2xl">Recent News</h2>
-		<Button href="/news" variant="outline" class="flex gap-x-2 rounded-full">
+		<Button href="#more-news" variant="outline" class="flex gap-x-2 rounded-full">
 			<p>View all</p>
 			<ArrowRight class="size-4" />
 		</Button>
@@ -49,4 +49,17 @@
 		</Carousel.Content>
 		<Carousel.Next />
 	</Carousel.Root>
+</div>
+
+<div id="more-news" class="bg-primary py-24 text-primary-foreground">
+	<div class="container mx-auto flex h-full flex-col justify-center gap-y-8">
+		<h2 class="text-center text-xl font-bold md:text-2xl">More News from UPD DCS</h2>
+		<div class="flex flex-wrap gap-5">
+			{#each news as news_item}
+				<div class="min-w-0 shrink-0 grow-0 basis-full md:basis-1/4">
+					<NewsCard {news_item} />
+				</div>
+			{/each}
+		</div>
+	</div>
 </div>
