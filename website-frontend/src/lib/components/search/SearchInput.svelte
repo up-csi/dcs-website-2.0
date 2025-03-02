e4929c0c394a16201b7be48370d53f4ce4297ee1
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-02 17:59:23 +0800

    fix(Search): fix search bar border

--- website-frontend/src/lib/components/search/SearchInput.svelte
+++ website-frontend/src/lib/components/search/SearchInput.svelte
@@ -15,12 +15,27 @@
     }
 
     let txt_area: any;
+    let txt_area_mobile: any;
 </script>
 
-<div class="h-fit w-full">
-    <form action="" class="rounded-3xl border lg:{border_display}">
+<div class="h-fit w-full hidden lg:block">
+    <form action="" class="rounded-3xl {border_display}">
         <div class="flex items-center w-full pr-1 pl-3">
             <input type="text" class="focus:outline-none w-full lg:{input_display}" bind:this={txt_area} on:input={() => { typing = true; }} />
+
+            {#if typing}
+                <button class="h-10 w-10 flex items-center justify-center mr-2" on:click={() => { txt_area.value = ""; typing = false; }}><X class="h-8 w-8 bg-secondary rounded-3xl p-1" /></button>
+            {:else}
+                <button type="submit" class="h-10 w-10 flex items-center justify-center" on:click={() => { search_open = !(search_open); }}><Search class="h-10 text-secondary hover:text-primary" /></button>
+            {/if}
+        </div>
+    </form>
+</div>
+
+<div class="h-fit w-full lg:hidden">
+    <form action="" class="rounded-3xl border">
+        <div class="flex items-center w-full pr-1 pl-3">
+            <input type="text" class="focus:outline-none w-full lg:{input_display}" bind:this={txt_area_mobile} on:input={() => { typing = true; }} />
 
             {#if typing}
                 <button class="h-10 w-10 flex items-center justify-center mr-2" on:click={() => { txt_area.value = ""; typing = false; }}><X class="h-8 w-8 bg-secondary rounded-3xl p-1" /></button>
