9960e94435c264059aafba2a026dc14f4b12899c
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-02 18:34:45 +0800

    feat(Search): clear mobile search bar on x press

--- website-frontend/src/lib/components/search/SearchInput.svelte
+++ website-frontend/src/lib/components/search/SearchInput.svelte
@@ -38,7 +38,7 @@
             <input type="text" class="focus:outline-none w-full lg:{input_display}" bind:this={txt_area_mobile} on:input={() => { typing = true; }} />
 
             {#if typing}
-                <button class="h-10 w-10 flex items-center justify-center mr-2" on:click={() => { txt_area.value = ""; typing = false; }}><X class="h-8 w-8 bg-secondary rounded-3xl p-1" /></button>
+                <button class="h-10 w-10 flex items-center justify-center mr-2" on:click={() => { txt_area_mobile.value = ""; typing = false; }}><X class="h-8 w-8 bg-secondary rounded-3xl p-1" /></button>
             {:else}
                 <button type="submit" class="h-10 w-10 flex items-center justify-center" on:click={() => { search_open = !(search_open); }}><Search class="h-10 text-secondary hover:text-primary" /></button>
             {/if}
