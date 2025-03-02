69fe0948503e7706e4f4bfb95f1306d0898d9548
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-02 18:39:00 +0800

    feat(Search): remove valibot parse from dummy data

--- website-frontend/src/routes/search/+page.server.ts
+++ website-frontend/src/routes/search/+page.server.ts
@@ -1,18 +1,18 @@
 /** @type {import('./$types').PageServerLoad} */
 import { readItems, readSingleton } from '@directus/sdk';
-import { parse } from 'valibot';
-import { Events } from '$lib/models/event';
-import { People } from '$lib/models/people';
-import { Laboratories } from '$lib/models/laboratories';
-import { Publications } from '$lib/models/publications';
+// import { parse } from 'valibot';
+// import { Events } from '$lib/models/event';
+// import { People } from '$lib/models/people';
+// import { Laboratories } from '$lib/models/laboratories';
+// import { Publications } from '$lib/models/publications';
 import getDirectusInstance from '$lib/directus';
 
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	return {
-		events: parse(Events, await directus.request(readItems('events'))),
-		people: parse(People, await directus.request(readItems('people'))),
-		laboratories: parse(Laboratories, await directus.request(readItems('laboratories'))),
-		publications: parse(Publications, await directus.request(readItems('publications'))),
+		events: await directus.request(readItems('events')),
+		people: await directus.request(readItems('people')),
+		laboratories: await directus.request(readItems('laboratories')),
+		publications: await directus.request(readItems('publications')),
 	};
 }
