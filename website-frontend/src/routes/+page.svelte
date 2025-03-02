2863e3bbf9c4e61371ed3dcd18dcb8646be283f8
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-02 22:26:54 +0800

    feat: add arrow to view all buttons in home page

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	/** @type {import('./$types').PageData} */
 	import * as Carousel from '$lib/@shadcn-svelte/ui/carousel';
+	import { Button } from '$lib/@shadcn-svelte/ui/button';
 	import LandingHero from '$lib/components/carousels/LandingHero.svelte';
 	import FeaturedEventCard from '$lib/components/events/FeaturedEventCard.svelte';
 	import NewsCard from '$lib/components/cards/NewsCard.svelte';
+	import { ArrowRight } from 'lucide-svelte';
 
 	export let data;
 
@@ -13,7 +15,13 @@
 <LandingHero {news} />
 
 <div class="container mx-auto my-8 flex h-full flex-col justify-center gap-y-5">
-	<h2 class="text-xl font-bold md:text-2xl">Recent News</h2>
+	<div class="flex justify-between">
+		<h2 class="text-xl font-bold md:text-2xl">Recent News</h2>
+		<Button href="/news" variant="outline" class="flex gap-x-2 rounded-full">
+			<p>View all</p>
+			<ArrowRight class="size-4" />
+		</Button>
+	</div>
 	<Carousel.Root>
 		<Carousel.Content>
 			{#each news as news_item}
@@ -24,7 +32,13 @@
 		</Carousel.Content>
 		<Carousel.Next />
 	</Carousel.Root>
-	<h2 class="text-xl font-bold md:text-2xl">Recent Events</h2>
+	<div class="flex justify-between">
+		<h2 class="text-xl font-bold md:text-2xl">Recent Events</h2>
+		<Button href="/events" variant="outline" class="flex gap-x-2 rounded-full">
+			<p>View all</p>
+			<ArrowRight class="size-4" />
+		</Button>
+	</div>
 	<Carousel.Root>
 		<Carousel.Content>
 			{#each events as event}
