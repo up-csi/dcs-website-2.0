e515693ef6662839b72dfd32963bbde9caa23873
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-03 01:22:02 +0800

    feat: align footer to project prototype specs

--- website-frontend/src/lib/components/footer/Footer.svelte
+++ website-frontend/src/lib/components/footer/Footer.svelte
@@ -10,34 +10,34 @@
 	export let x_link;
 </script>
 
-<div class="bg-secondary p-10 text-secondary-foreground lg:grid lg:grid-cols-3 lg:gap-4">
+<div class="bg-primary p-10 text-primary-foreground lg:grid lg:grid-cols-3 lg:gap-4">
 	<div class="my-4 lg:mx-2 lg:my-0">
-		<p class="py-2 text-2xl font-bold text-foreground lg:text-xl">Department of Computer Science</p>
+		<p class="py-2 text-2xl font-bold lg:text-xl">Department of Computer Science</p>
 		{#each address as { address_line }}
 			<p>{address_line}</p>
 		{/each}
 		<p>
 			{contact_number} |
-			<a href="mailto:{contact_email}" class="transition-colors duration-300 hover:text-primary"
+			<a href="mailto:{contact_email}" class="transition-colors duration-300 hover:text-secondary"
 				>{contact_email}</a
 			>
 		</p>
 	</div>
 	<div class="my-4 lg:mx-2 lg:my-0">
-		<p class="py-2 text-xl font-bold text-foreground">Quick Links</p>
+		<p class="py-2 text-xl font-bold">Quick Links</p>
 		{#each quick_links as { name, link }}
-			<a href={link} class="transition-colors duration-300 hover:text-primary">{name}</a>
+			<a href={link} class="transition-colors duration-300 hover:text-secondary">{name}</a>
 			<br />
 		{/each}
 	</div>
 	<div class="my-4 lg:mx-2 lg:my-0">
-		<p class="py-2 text-xl font-bold text-foreground">Follow Us On</p>
+		<p class="py-2 text-xl font-bold">Follow Us On</p>
 		<div class="flex items-center">
 			<a
 				href={facebook_link}
 				target="_blank"
 				rel="noopener noreferrer"
-				class="mr-3 h-10 pb-[2px] transition-colors duration-300 hover:text-primary"
+				class="mr-3 h-10 pb-[2px] transition-colors duration-300 hover:text-secondary"
 			>
 				<FacebookIcon />
 			</a>
@@ -45,7 +45,7 @@
 				href={x_link}
 				target="_blank"
 				rel="noopener noreferrer"
-				class="h-8 transition-colors duration-300 hover:text-primary"
+				class="h-8 transition-colors duration-300 hover:text-secondary"
 			>
 				<XIcon />
 			</a>
@@ -53,5 +53,5 @@
 	</div>
 </div>
 <div class="flex justify-center bg-primary text-primary-foreground">
-	<p class="my-6 text-sm">&copy;2024 UP Department of Computer Science</p>
+	<p class="my-6 text-sm">&copy; 2024 UP Department of Computer Science</p>
 </div>
