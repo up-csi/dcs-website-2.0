59e3d2477717c6c0d541a7913a3b011cbb1b9ce9
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-03 09:22:16 +0800

    feat: add initial news details styling

--- website-frontend/src/routes/news/[slug]/+page.svelte
+++ website-frontend/src/routes/news/[slug]/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	/** @type {import('./$types').PageData} */
-	import Banner from '$lib/components/banners/Banner.svelte';
+	import { Button } from '$lib/@shadcn-svelte/ui/button';
+	import { PUBLIC_APIURL } from '$env/static/public';
 	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
+	import { Share } from 'lucide-svelte';
 	export let data;
 
 	$: ({ news_item } = data);
@@ -9,22 +11,54 @@
 
 <body>
 	{#if news_item}
-		<!-- TODO: Create a dedicated news banner and frontend page formatting -->
-		<Banner
-			title={news_item.title}
-			flexible_content="{news_item.summary} <br /><br /> Written by {news_item.user_created
-				.first_name}
-			{news_item.user_created.last_name} | {news_item.date_created}"
-			from_bottom={'bottom-40'}
-		/>
-		<br />
-
-		<div class="px-4 py-10 text-base md:px-32">
-			{#if news_item.flexible_content}
-				<FlexibleContent content={news_item.flexible_content} />
-			{:else}
-				<p>Page is empty.</p>
+		<div class="flex flex-col gap-y-5">
+			{#if news_item.background_image}
+				<div class="mt-24 h-full px-5">
+					<img
+						class="h-[60svh] w-full object-cover"
+						src="{PUBLIC_APIURL}/assets/{news_item.background_image}"
+						alt="Background"
+					/>
+				</div>
 			{/if}
+			<div
+				class="container mx-auto flex flex-col justify-center gap-y-5 pb-16"
+				class:mt-8={news_item.background_image}
+				class:mt-24={!news_item.background_image}
+			>
+				<h1 class="text-6xl font-bold">{news_item.title}</h1>
+				<p class="text-xl">{news_item.summary}</p>
+				<div class="flex justify-between">
+					<div class="flex flex-col">
+						<small
+							>Written by {news_item.user_created.first_name}
+							{news_item.user_created.last_name}</small
+						>
+						<small class="text-gray-500"
+							>Published on {new Date(news_item.date_created).toLocaleDateString('en-US', {
+								month: 'long',
+								day: 'numeric',
+								year: 'numeric'
+							})} | {new Date(news_item.date_created).toLocaleTimeString('en-US', {
+								timeZone: 'PST',
+								timeZoneName: 'short'
+							})}
+						</small>
+					</div>
+					<Button variant="outline" class="flex gap-x-2 rounded-full"
+						>Share <Share class="size-4" /></Button
+					>
+				</div>
+			</div>
+		</div>
+		<div class="bg-gray-100 pb-24 pt-16">
+			<div class="container">
+				{#if news_item.flexible_content}
+					<FlexibleContent content={news_item.flexible_content} />
+				{:else}
+					<p>Page is empty.</p>
+				{/if}
+			</div>
 		</div>
 	{:else}
 		<p>Page not found</p>
