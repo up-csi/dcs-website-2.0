05bdffdd4d90c05e8d318ec427287f175332d1e9
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-05 17:04:38 +0800

    feat: add breadcrumbs to people subpages

--- website-frontend/src/routes/people/[slug]/+page.svelte
+++ website-frontend/src/routes/people/[slug]/+page.svelte
@@ -2,28 +2,12 @@
 	/** @type {import('./$types').PageData} */
 	import Banner from '$lib/components/banners/Banner.svelte';
 	import CardPanel from '$lib/components/panel/CardPanel.svelte';
-	import FilterBar from '$lib/components/filter/FilterBar.svelte';
-	import LoadMore from '$lib/components/buttons/LoadMore.svelte';
 	import PeopleCard from '$lib/components/cards/PeopleCard.svelte';
+	import { deslugify } from '$lib/utils';
 
 	export let data;
 
-	$: ({ category, people, position_filters, laboratory_filters } = data);
-
-	const inc = 12;
-	let shown = inc;
-	$: peopleList = people?.slice(0, shown);
-
-	$: controls = [
-		{
-			name: 'position',
-			categories: position_filters
-		},
-		{
-			name: 'laboratory',
-			categories: laboratory_filters
-		}
-	];
+	$: ({ category, people } = data);
 </script>
 
 <body>
@@ -36,22 +20,26 @@
 			/>
 		</div>
 
-		<div class="relative z-10 -mt-7">
-			<FilterBar {controls} />
-		</div>
-
-		<CardPanel>
-			{#each peopleList as person}
-				<a href="/people/{category.title}/{person.username}">
-					<PeopleCard {person} laboratory={person.affiliations?.[0]?.laboratories_id?.name} />
-				</a>
-			{/each}
-			{#if shown < people.length}
-				<div class="col-span-full mt-8 flex items-center justify-center">
-					<LoadMore {inc} bind:shown />
+		<div class="w-[94vw] md:w-[80vw] mx-auto">
+			<div>
+				<div class="flex space-x-2 pt-5 pb-2 text-xs font-medium">
+					<a class="" href="/">Home</a>
+					<p class="opacity-55">/</p>
+					<a class="" href="/people">People</a>
+					<p class="opacity-55">/</p>
+					<p class="opacity-55">{deslugify(category.title)}</p>
 				</div>
-			{/if}
-		</CardPanel>
+				<div class="h-[1px] w-full bg-primary opacity-20"></div>
+			</div>
+		
+			<CardPanel>
+				{#each people as person}
+					<a href="/people/{category.title}/{person.username}">
+						<PeopleCard {person} laboratory={person.affiliations?.[0]?.laboratories_id?.name} />
+					</a>
+				{/each}
+			</CardPanel>
+		</div>
 	{:else}
 		<p>People category not found</p>
 	{/if}
