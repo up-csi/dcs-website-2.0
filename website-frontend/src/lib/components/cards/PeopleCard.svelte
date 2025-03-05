afd7237e6fa65244fb86067d260e0a8b91a5d94b
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-05 16:00:08 +0800

    feat: make ui adjustments to PeopleCard

--- website-frontend/src/lib/components/cards/PeopleCard.svelte
+++ website-frontend/src/lib/components/cards/PeopleCard.svelte
@@ -6,11 +6,11 @@
 	export let laboratory: string;
 </script>
 
-<div class="card relative flex h-full flex-col overflow-hidden md:h-[380px]">
-	<div class="inset-0 h-24 rounded-t-lg bg-gray-300 md:h-40">
+<div class="card relative flex h-full flex-col overflow-hidden w-full group">
+	<div class="inset-0 h-28 rounded-t-lg bg-gray-300 md:h-44 overflow-hidden">
 		{#if person.background_image}
 			<img
-				class="h-full w-full rounded-t-lg object-cover"
+				class="h-full w-full rounded-t-lg object-cover transition-transform duration-300 ease-out group-hover:scale-105"
 				src="{PUBLIC_APIURL}/assets/{person.background_image}"
 				alt="Background"
 			/>
@@ -18,9 +18,10 @@
 	</div>
 
 	<div class="z-10 -mt-16 items-center px-3 md:-mt-20">
-		<div
-			class="mx-auto flex h-28 w-28 items-center justify-center rounded-full border-4 border-gray-200 bg-gray-100 md:h-32 md:w-32"
-		>
+		<div class="relative mx-auto flex h-28 w-28 items-center justify-center rounded-full md:h-32 md:w-32">
+			
+			<div class="absolute -inset-[2px] md:-inset-1 -z-10 rounded-full border-2 md:border-4 border-primary/20 backdrop-blur-lg"></div>
+		
 			{#if person.profile_image}
 				<img
 					class="h-full w-full rounded-full object-cover"
@@ -32,16 +33,16 @@
 			{/if}
 		</div>
 
-		<div class="p-3 text-center md:mb-3 md:mt-2">
-			<div class="text-xs font-semibold uppercase leading-tight text-gray-500">
+		<div class="p-3 mt-2 mb-3 text-center">
+			<div class="text-xs font-semibold uppercase leading-none md:leading-tight opacity-55">
 				<p>{person.position}</p>
 			</div>
-			<h3 class="pb-2 font-bold leading-tight text-gray-900 md:py-1 md:text-xl">
+			<h3 class="py-2 font-bold leading-none text-gray-900 md:text-xl">
 				{person.first_name}
 				{person.last_name}
 			</h3>
 			{#if laboratory}
-				<div class="flex items-center justify-center text-xs text-gray-500 md:px-6">
+				<div class="text-xs font-medium leading-none md:leading-tight opacity-55 md:px-6">
 					<p class="line-clamp-2 w-full">{laboratory}</p>
 				</div>
 			{/if}
