c72cfff1373f6155cbdd4856b0e1e7c189eb1594
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-05 17:17:44 +0800

    refactor: clean up redundant heading stylings

--- website-frontend/src/routes/people/+page.svelte
+++ website-frontend/src/routes/people/+page.svelte
@@ -11,6 +11,8 @@
 	$: regularList = people?.filter((person: Person) => person.category === "regular-faculty") ?? [];
 	$: lecturerList = people?.filter((person: Person) => person.category === "lecturers-and-teaching-associates") ?? [];
 	$: supportList = people?.filter((person: Person) => person.category === "support-staff") ?? [];
+
+	let heading = `text-2xl md:text-3xl font-bold inline-block pb-3 md:pb-4 leading-none`;
 </script>
 
 <body>
@@ -22,7 +24,6 @@
 		/>
 	</div>
 
- 
 	<div class="w-[94vw] md:w-[80vw] mx-auto">
 
 		<div>
@@ -36,7 +37,7 @@
 
 		<div class="space-y-4 py-8 md:py-10">
 			{#if regularList.length > 0}
-				<a class="text-2xl md:text-3xl font-bold inline-block pb-3 md:pb-4 leading-none" href="/people/regular-faculty">Regular Faculty</a>
+				<a class="{heading}" href="/people/regular-faculty">Regular Faculty</a>
 				<CardPanel>
 					{#each regularList as person}
 						<a href="/people/{person.category}/{person.username}">
@@ -46,7 +47,7 @@
 				</CardPanel>
 			{/if}
 			{#if lecturerList.length > 0}
-				<a class="text-2xl md:text-3xl font-bold inline-block pb-3 md:pb-4 leading-none" href="/people/lecturers-and-teaching-associates">Lecturers & Teaching Associates</a>
+				<a class="{heading}" href="/people/lecturers-and-teaching-associates">Lecturers & Teaching Associates</a>
 				<CardPanel>
 					{#each lecturerList as person}
 						<a href="/people/{person.category}/{person.username}">
@@ -56,7 +57,7 @@
 				</CardPanel>
 			{/if}
 			{#if supportList.length > 0}
-				<a class="text-2xl md:text-3xl font-bold inline-block pb-3 md:pb-4 leading-none" href="/people/support-staff">Support Staff</a>
+				<a class="{heading}" href="/people/support-staff">Support Staff</a>
 				<CardPanel>
 					{#each supportList as person}
 						<a href="/people/{person.category}/{person.username}">
