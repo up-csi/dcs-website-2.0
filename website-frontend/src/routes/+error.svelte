1d85af804c536096182d6a4dd97c48faae16b953
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-05 20:33:30 +0800

    fix: change styling of error

--- website-frontend/src/routes/+error.svelte
+++ website-frontend/src/routes/+error.svelte
@@ -2,12 +2,13 @@
     import { page } from "$app/state";
 </script>
 
+<div class="prose container mt-20 flex flex-col items-center">
+    <h1>{page.status}: {page.error?.message}</h1>
 
-
-<div class="prose container mt-20">
-    <h1>{page.status}</h1>
-
-    <h3>{page.error?.message}</h3>
-
-    <aside><p>"Success is not final, failure is not fatal: it is the courage to continue that counts." - Winston Churchill</p></aside>
+    <aside>
+        <blockquote>
+            "Success is not final, failure is not fatal: it is the courage to continue that counts."
+        </blockquote>
+        <cite>- Winston Churchill</cite>
+    </aside>
 </div>
