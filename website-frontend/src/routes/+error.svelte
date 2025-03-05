684a90a360eb144b2e19e31ab86735dee7aa0cfb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-03-05 20:14:08 +0800

    feat: add error page

---
+++ website-frontend/src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
+
+
+<div class="prose container mt-20">
+    <h1>{page.status}</h1>
+
+    <h3>{page.error?.message}</h3>
+
+    <aside><p>"Success is not final, failure is not fatal: it is the courage to continue that counts." - Winston Churchill</p></aside>
+</div>
