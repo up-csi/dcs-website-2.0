982abc801b9ad92fcddb38cc626d1273e9e7c023
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-06 19:30:27 +0800

    feat(PageBreadcrumb): restyle page breadcrumbs

--- website-frontend/src/lib/components/banners/PageBreadcrumb.svelte
+++ website-frontend/src/lib/components/banners/PageBreadcrumb.svelte
@@ -19,12 +19,12 @@
     });
 
     const to_current_path: NavPaths = nav_paths.slice(0, -1);
-    const current_path = nav_paths[nav_paths.length - 1];
+    const current_page = nav_paths[nav_paths.length - 1].to;
 </script>
 
-<div class="hidden lg:block pb-2 border-b-2">
+<div class="hidden lg:block pb-2 border-b border-muted-foreground/40">
     <Breadcrumb.Root>
-        <Breadcrumb.List class="hover:text-primary">
+        <Breadcrumb.List class="hover:text-primary text-xs font-medium">
             <Breadcrumb.Item class="text-foreground">
                 <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
             </Breadcrumb.Item>
@@ -34,19 +34,19 @@
                     <Breadcrumb.Link {href}>{to}</Breadcrumb.Link>
                 </Breadcrumb.Item>
             {/each}
-            {#if current_path}
+            {#if current_page}
                 <Breadcrumb.Separator>/</Breadcrumb.Separator>
                 <Breadcrumb.Item>
-                    <Breadcrumb.Link href={current_path.href}>{current_path.to}</Breadcrumb.Link>
+                    <Breadcrumb.Page class="text-muted-foreground/80 font-medium">{current_page}</Breadcrumb.Page>
                 </Breadcrumb.Item>
             {/if}
         </Breadcrumb.List>
     </Breadcrumb.Root>
 </div>
 
-<div class="lg:hidden pb-2 border-b-2">
+<div class="lg:hidden pb-2 border-b border-muted-foreground/40">
     <Breadcrumb.Root>
-        <Breadcrumb.List class="hover:text-primary">
+        <Breadcrumb.List class="hover:text-primary text-xs font-medium">
             <Breadcrumb.Item class="text-foreground">
                 <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
             </Breadcrumb.Item>
@@ -68,10 +68,10 @@
                     </DropdownMenu.Root>
                 </Breadcrumb.Item>
             {/if}
-            {#if current_path}
+            {#if current_page}
                 <Breadcrumb.Separator>/</Breadcrumb.Separator>
                 <Breadcrumb.Item>
-                    <Breadcrumb.Link href={current_path.href}>{current_path.to}</Breadcrumb.Link>
+                    <Breadcrumb.Page class="text-muted-foreground/80 font-medium">{current_page}</Breadcrumb.Page>
                 </Breadcrumb.Item>
             {/if}
         </Breadcrumb.List>
