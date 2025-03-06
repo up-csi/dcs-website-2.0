d252a338039b00414d3f04511874544170225015
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-06 20:10:20 +0800

    feat(About): add breadcrumbs

--- website-frontend/src/routes/about/+page.svelte
+++ website-frontend/src/routes/about/+page.svelte
@@ -2,6 +2,7 @@
 	/** @type {import('./$types').PageData} */
 	import Banner from '$lib/components/banners/Banner.svelte';
 	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
+	import Breadcrumb from '$lib/components/breadcrumbs/PageBreadcrumb.svelte';
 	export let data;
 
 	const { about } = data;
@@ -12,6 +13,10 @@
 		<Banner title="About" />
 
 		<div class="px-4 py-10 text-base md:px-32">
+			<div class="mb-5">
+				<Breadcrumb />
+			</div>
+
 			{#if about.flexible_content}
 				<FlexibleContent content={about.flexible_content} />
 			{:else}
