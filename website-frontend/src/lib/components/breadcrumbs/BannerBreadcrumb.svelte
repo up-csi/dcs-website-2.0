0a69c7ae39fe1dc4bee9d223088f7ebe47f2002d
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-06 19:15:26 +0800

    feat: prefer separate component for banner breadcrumbs

---
+++ website-frontend/src/lib/components/breadcrumbs/BannerBreadcrumb.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    import { deslugify } from '$lib/utils';
+    import { page } from '$app/stores';
+	import * as Breadcrumb from '$lib/@shadcn-svelte/ui/breadcrumb/index.js';
+	import * as DropdownMenu from '$lib/@shadcn-svelte/ui/dropdown-menu/index.js';
+    import type { NavPaths } from '$lib/types/path';
+    import { ChevronDown } from 'lucide-svelte';
+
+    export let page_name;
+
+    const whole_path = $page.url.pathname.split('/').slice(1);
+    let app_path = '';
+
+    let nav_paths: NavPaths = [];
+    whole_path.forEach(path_end => {
+        app_path = app_path.concat('/' + path_end);
+        nav_paths.push({
+            to: deslugify(path_end),
+            href: app_path,
+        });
+    });
+
+    const to_current_path: NavPaths = nav_paths.slice(0, -1);
+    const current_path = nav_paths[nav_paths.length - 1];
+</script>
+
+<div class="hidden lg:block pb-4 border-b-2 border-secondary-foreground/40">
+    <Breadcrumb.Root>
+        <Breadcrumb.List class="text-background">
+            <Breadcrumb.Item class="hover:text-primary">
+                <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+            </Breadcrumb.Item>
+            {#each to_current_path as { to, href }}
+                <Breadcrumb.Separator>/</Breadcrumb.Separator>
+                <Breadcrumb.Item class="hover:text-primary">
+                    <Breadcrumb.Link {href}>{to}</Breadcrumb.Link>
+                </Breadcrumb.Item>
+            {/each}
+            {#if current_path}
+                <Breadcrumb.Separator>/</Breadcrumb.Separator>
+                <Breadcrumb.Item>
+                    <Breadcrumb.Page class="text-[#D9D9D9]">{page_name}</Breadcrumb.Page>
+                </Breadcrumb.Item>
+            {/if}
+        </Breadcrumb.List>
+    </Breadcrumb.Root>
+</div>
+
+<div class="lg:hidden pb-2 border-b-2 border-secondary-foreground/40">
+    <Breadcrumb.Root>
+        <Breadcrumb.List class="text-background">
+            <Breadcrumb.Item class="hover:text-primary">
+                <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+            </Breadcrumb.Item>
+            {#if to_current_path}
+                <Breadcrumb.Separator>/</Breadcrumb.Separator>
+                <Breadcrumb.Item class="hover:text-primary">
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger class="flex items-center gap-1">
+                            <Breadcrumb.Ellipsis />
+                            <ChevronDown class="h-4 w-4" />
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content align="start">
+                            {#each to_current_path as { to, href }}
+                                <DropdownMenu.Item>
+                                    <a {href}>{to}</a>
+                                </DropdownMenu.Item>
+                            {/each}
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                </Breadcrumb.Item>
+            {/if}
+            {#if current_path}
+                <Breadcrumb.Separator>/</Breadcrumb.Separator>
+                <Breadcrumb.Item>
+                    <Breadcrumb.Page class="text-[#D9D9D9]">{page_name}</Breadcrumb.Page>
+                </Breadcrumb.Item>
+            {/if}
+        </Breadcrumb.List>
+    </Breadcrumb.Root>
+</div>
