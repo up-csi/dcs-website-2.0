4df07254a74e66034399b0e0a8c6ba4d98f01448
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-07 09:55:55 +0800

    fix: use stores for managing search and mobile navbar open states

--- website-frontend/src/lib/components/search/SearchInput.svelte
+++ website-frontend/src/lib/components/search/SearchInput.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
 	import { Search, X } from 'lucide-svelte';
-
-	export let search_open = false;
-	let searchInput = '';
+	import { mobileOpen, searchOpen, searchInput } from '$lib/stores';
 </script>
 
 <div class="hidden h-fit w-full lg:block">
-	<form action="/search" class="rounded-3xl {search_open ? 'border' : 'border-0'}">
+	<form
+		action="/search"
+		class="rounded-3xl {$searchOpen ? 'border' : 'border-0'}"
+		on:submit={() => {
+			$mobileOpen = false;
+		}}
+	>
 		<div class="flex w-full items-center pl-3 pr-1">
 			<input
 				name="q"
 				type="text"
-				class="w-full focus:outline-none lg:{search_open ? 'block' : 'hidden'}"
-				bind:value={searchInput}
-				on:submit={() => {
-					search_open = false;
-					searchInput = '';
-				}}
+				class="w-full focus:outline-none lg:{$searchOpen ? 'block' : 'hidden'}"
+				bind:value={$searchInput}
 			/>
 
-			{#if searchInput}
+			{#if $searchOpen}
 				<button
 					type="button"
 					class="mr-2 flex h-10 w-10 items-center justify-center"
 					on:click={() => {
-						search_open = !search_open;
-						searchInput = '';
+						$searchOpen = !$searchOpen;
 					}}><X class="h-8 w-8 rounded-3xl bg-secondary p-1" /></button
 				>
 			{:else}
@@ -33,7 +32,7 @@
 					type="button"
 					class="flex h-10 w-10 items-center justify-center"
 					on:click={() => {
-						search_open = !search_open;
+						$searchOpen = !$searchOpen;
 					}}><Search class="h-10 text-secondary hover:text-primary" /></button
 				>
 			{/if}
@@ -42,36 +41,33 @@
 </div>
 
 <div class="h-fit w-full lg:hidden">
-	<form action="/search" class="rounded-3xl border">
+	<form
+		action="/search"
+		class="rounded-3xl border"
+		on:submit={() => {
+			$mobileOpen = false;
+		}}
+	>
 		<div class="flex w-full items-center pl-3 pr-1">
 			<input
 				name="q"
 				type="text"
-				class="w-full focus:outline-none lg:{search_open ? 'block' : 'hidden'}"
-				bind:value={searchInput}
-				on:submit={() => {
-					search_open = false;
-					searchInput = '';
-				}}
+				class="w-full focus:outline-none lg:{$searchOpen ? 'block' : 'hidden'}"
+				bind:value={$searchInput}
 			/>
 
-			{#if searchInput}
+			{#if $searchInput}
 				<button
 					type="button"
 					class="mr-2 flex h-10 w-10 items-center justify-center"
 					on:click={() => {
-						search_open = !search_open;
-						searchInput = '';
+						$searchInput = '';
 					}}><X class="h-8 w-8 rounded-3xl bg-secondary p-1" /></button
 				>
 			{:else}
-				<button
-					type="button"
-					class="flex h-10 w-10 items-center justify-center"
-					on:click={() => {
-						search_open = !search_open;
-					}}><Search class="h-10 text-secondary hover:text-primary" /></button
-				>
+				<div class="flex h-10 w-10 items-center justify-center">
+					<Search class="h-10 text-secondary hover:text-primary" />
+				</div>
 			{/if}
 		</div>
 	</form>
