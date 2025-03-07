4b9d2137e73441b6d6a89353f6ed69c89b33fe06
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-07 23:06:51 +0800

    feat: redesign DataTable into CoursesTable

---
+++ website-frontend/src/lib/components/buttons/LinkButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button } from '$lib/@shadcn-svelte/ui/button';
+    import { ChevronRight } from 'lucide-svelte';
+
+    export let link: string = '';
+    export let text: string = '';
+</script>
+
+<Button
+	class="rounded-full"
+	variant="outline"
+	href={link}
+>
+	<p class="text-xs font-semibold flex items-center gap-1">
+		{text}
+		<ChevronRight class="w-3 h-3" />
+	</p>
+</Button>
---
+++ website-frontend/src/lib/components/table/CoursesTable.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import * as Table from '$lib/@shadcn-svelte/ui/table';
+	import { deslugify } from '$lib/utils';
+
+	export let data: Array<object>;
+
+	const hiddenColumns = ["user_created", "date_created", "user_updated", "date_updated"];
+</script>
+
+{#if data.length !== 0}
+	<div class="shadow-lg">
+		<Table.Root class="rounded-lg overflow-hidden">
+			<Table.Header class="bg-gray-100">
+				<Table.Row>
+					{#each Object.keys(data[0]).filter(col => !hiddenColumns.includes(col)) as heading}
+						<Table.Head class="px-4 py-2 text-left text-sm font-semibold bg-gray-100 text-gray-700">
+							{deslugify(heading)}
+						</Table.Head>
+					{/each}
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{#each data as item}
+					<Table.Row>
+						{#each Object.entries(item).filter(([key]) => !hiddenColumns.includes(key)) as [_, value]}
+							<Table.Cell class="bg-white text-gray-600">
+								{value}
+							</Table.Cell>
+						{/each}
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
+{:else}
+	<br />
+	Empty
+	<br />
+{/if}
