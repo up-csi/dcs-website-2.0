8c124d4f9bf17eb2e1a1a85d78eb94632ba6e777
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-08 01:06:39 +0800

    feat: add working category slug pages

--- website-frontend/src/routes/academics/+page.svelte
+++ website-frontend/src/routes/academics/+page.svelte
@@ -3,7 +3,7 @@
 	import Banner from '$lib/components/banners/Banner.svelte';
 	import LinkButton from '$lib/components/buttons/LinkButton.svelte';
 	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
-	import DataTable from '$lib/components/table/CoursesTable.svelte';
+	import CoursesTable from '$lib/components/table/CoursesTable.svelte';
 	import Breadcrumb from '$lib/components/breadcrumbs/PageBreadcrumb.svelte';
 
 	export let data;
@@ -31,43 +31,45 @@
 		</div>
 
 		<div class="flex justify-center px-4">
-			<div class="pb-20 md:pb-28 max-w-6xl w-full">
+			<div class="pb-16 md:pb-24 max-w-6xl w-full">
+				<div class="space-y-10 md:space-y-16">
 
-				<div>
-					<div class="flex space-x-2 pb-2 pt-5 text-xs font-medium">
-						<a class="" href="/">Home</a>
-						<p class="opacity-55">/</p>
-						<p class="opacity-55">Academics</p>
+					<div>
+						<div class="flex space-x-2 pb-2 pt-5 text-xs font-medium">
+							<a class="" href="/">Home</a>
+							<p class="opacity-55">/</p>
+							<p class="opacity-55">Academics</p>
+						</div>
+						<div class="h-[1px] w-full bg-primary opacity-20"></div>
 					</div>
-					<div class="h-[1px] w-full bg-primary opacity-20"></div>
-				</div>
 
-				<div class="prose pt-10 text-base">
-					{#if academics.flexible_content}
-						<FlexibleContent content={academics.flexible_content} />
-					{:else}
-						<p>Page is empty.</p>
-					{/if}
-				</div>
+					<div class="prose text-base">
+						{#if academics.flexible_content}
+							<FlexibleContent content={academics.flexible_content} />
+						{:else}
+							<p>Page is empty.</p>
+						{/if}
+					</div>
 
-				<h1 class="mt-10 mb-6 text-3xl font-bold">
-					Programs offered by the department
-				</h1>
-				<div class="space-y-1">
-					<LinkButton text="Go To Undergraduate Page" link="academics/undergraduate" />
-					<LinkButton text="Go To Graduate Page" link="academics/graduate" />
-				</div>
+					<div>
+						<h1 class="mb-3 md:mb-6 text-2xl md:text-3xl leading-tight font-bold">
+							Programs offered by the department
+						</h1>
+						<div class="space-y-1">
+							<LinkButton text="Undergraduate Page" link="academics/undergraduate" />
+							<LinkButton text="Graduate Page" link="academics/graduate" />
+						</div>
+					</div>
 
-				<div class="mt-16 mb-10">
-					<h1 class="text-3xl font-bold">
-						Courses offered by the department
-					</h1>
-					<p class="mt-2 ml-[2px] text-xs font-medium opacity-55">
-						Last Updated: March XX, 2025
-					</p>
-				</div>
-				<DataTable data={academics_courses} />
+					<div>
+						<div class="mb-6 md:mb-10">
+							<h1 class="text-2xl md:text-3xl leading-tight font-bold"> Courses offered by the department </h1>
+							<p class="mt-2 ml-[2px] text-xs font-medium opacity-55"> Last Updated: March XX, 2025 </p>
+						</div>
+						<CoursesTable data={academics_courses} />
+					</div>
 
+				</div>
 			</div>
 		</div>
 
