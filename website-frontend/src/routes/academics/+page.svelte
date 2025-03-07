f781ca4503f9f09efb125425b640b221e628aab2
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-07 23:06:51 +0800

    feat: redesign DataTable into CoursesTable

--- website-frontend/src/routes/academics/+page.svelte
+++ website-frontend/src/routes/academics/+page.svelte
@@ -1,13 +1,13 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	import Banner from '$lib/components/banners/Banner.svelte';
+	import LinkButton from '$lib/components/buttons/LinkButton.svelte';
 	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
-	import HorizontalCard from '$lib/components/cards/HorizontalCard.svelte';
-	import DataTable from '$lib/components/table/DataTable.svelte';
+	import DataTable from '$lib/components/table/CoursesTable.svelte';
 	import Breadcrumb from '$lib/components/breadcrumbs/PageBreadcrumb.svelte';
 
 	export let data;
-	$: ({ academics, academics_programs, academics_courses } = data);
+	$: ({ academics, academics_courses } = data);
 </script>
 
 <body>
@@ -22,35 +22,48 @@
 			<Breadcrumb />
 		</div>
 
-		<div class="px-4 md:px-32">
-			<div class="prose py-10 text-base">
-				{#if academics.flexible_content}
-					<FlexibleContent content={academics.flexible_content} />
-				{:else}
-					<p>Page is empty.</p>
-				{/if}
-			</div>
+		<div class="container mx-auto my-8 mb-5">
+			<Breadcrumb />
+		</div>
 
-			<h1 class="mb-8 text-3xl font-bold text-center">
-				List of programs offered by the department
-			</h1>
+		<div class="container mx-auto my-8 mb-5">
+			<Breadcrumb />
+		</div>
 
-			<div class="space-y-3">
-				{#each academics_programs as program}
-					{#if typeof program.category !== 'string'}
-						<a href="academics/{program.category.slug}/programs/{program.slug}" class="block">
-							<HorizontalCard name={program.title}/>
-						</a>
+		<div class="flex justify-center px-4">
+			<div class="pb-20 md:pb-28 max-w-6xl w-full">
+
+				<div>
+					<div class="flex space-x-2 pb-2 pt-5 text-xs font-medium">
+						<a class="" href="/">Home</a>
+						<p class="opacity-55">/</p>
+						<p class="opacity-55">Academics</p>
+					</div>
+					<div class="h-[1px] w-full bg-primary opacity-20"></div>
+				</div>
+
+				<div class="prose pt-10 text-base">
+					{#if academics.flexible_content}
+						<FlexibleContent content={academics.flexible_content} />
+					{:else}
+						<p>Page is empty.</p>
 					{/if}
-				{/each}
+				</div>
+
+				<h1 class="mt-10 mb-6 text-3xl font-bold">
+					Programs offered by the department
+				</h1>
+				<div class="space-y-1">
+					<LinkButton text="Go To Undergraduate Page" link="academics/undergraduate" />
+					<LinkButton text="Go To Graduate Page" link="academics/graduate" />
+				</div>
+
+				<h1 class="mt-12 mb-8 text-3xl font-bold">
+					Courses offered by the department
+				</h1>
+				<DataTable data={academics_courses} />
+
 			</div>
-
-			<br />
-
-			<h1 class="mb-8 text-3xl font-bold text-center">
-				List of courses offered by the department
-			</h1>
-			<DataTable data={academics_courses} />
 		</div>
 
 	{:else}
--- website-frontend/src/routes/academics/graduate/+page.svelte
+++ website-frontend/src/routes/academics/graduate/+page.svelte
@@ -2,7 +2,7 @@
 	/** @type {import('./$types').PageData} */
 	import Banner from '$lib/components/banners/Banner.svelte';
 	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
-	import DataTable from '$lib/components/table/DataTable.svelte';
+	import HorizontalCard from '$lib/components/cards/HorizontalCard.svelte';
 
 	export let data;
 	$: ({ academics, academics_programs, academics_courses } = data);
@@ -10,32 +10,51 @@
 
 <body>
 	{#if academics}
-		<Banner title="Academics" />
+		<Banner title="Graduate" />
 
-		<div class="prose px-4 py-10 text-base md:px-32">
-			{#if academics.flexible_content}
-				<FlexibleContent content={academics.flexible_content} />
-			{:else}
-				<p>Page is empty.</p>
-			{/if}
+		<div class="px-4 md:px-32">
+			<div>
+				<div class="flex space-x-2 pb-2 pt-5 text-xs font-medium">
+					<a class="" href="/">Home</a>
+					<p class="opacity-55">/</p>
+					<a class="" href="/academics">Academics</a>
+					<p class="opacity-55">/</p>
+					<p class="opacity-55">Graduate</p>
+				</div>
+				<div class="h-[1px] w-full bg-primary opacity-20"></div>
+			</div>
+
+			<div class="prose pt-10 text-base">
+				{#if academics.flexible_content}
+					<FlexibleContent content={academics.flexible_content} />
+				{:else}
+					<p>Page is empty.</p>
+				{/if}
+			</div>
+
+			<h1 class="mt-10 mb-8 text-3xl font-bold">
+				Graduate Programs
+			</h1>
+
+			<div class="space-y-3">
+				{#each academics_programs as program}
+					{#if typeof program.category !== 'string' && program.category.slug == 'graduate'}
+						<a href="academics/{program.category.slug}/programs/{program.slug}" class="block">
+							<HorizontalCard name={program.title}/>
+						</a>
+					{/if}
+				{/each}
+			</div>
+
+			<h1 class="mt-14 mb-8 text-3xl font-bold">
+				Admission Procedure
+			</h1>
+
+			<h1 class="mt-14 mb-8 text-3xl font-bold">
+				Forms
+			</h1>
 		</div>
 
-		<h1 class="mb-8 px-4 text-3xl font-bold text-gray-900 md:px-16">
-			List of programs offered by the department
-		</h1>
-		{#each academics_programs as program}
-			{#if typeof program.category !== 'string'}
-				<ul>
-					<a href="academics/{program.category.slug}/programs/{program.slug}">{program.title}</a>
-				</ul>
-			{/if}
-		{/each}
-		<br />
-
-		<h1 class="mb-8 px-4 text-3xl font-bold text-gray-900 md:px-16">
-			List of courses offered by the department
-		</h1>
-		<DataTable data={academics_courses} />
 	{:else}
 		<p>Page not found</p>
 	{/if}
