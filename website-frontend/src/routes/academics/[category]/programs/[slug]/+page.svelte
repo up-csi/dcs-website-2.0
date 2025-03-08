9a8774954d399101ac07c0259ade793a2c4d9215
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-08 12:19:43 +0800

    feat: add styling to program pages

--- website-frontend/src/routes/academics/[category]/programs/[slug]/+page.svelte
+++ website-frontend/src/routes/academics/[category]/programs/[slug]/+page.svelte
@@ -17,49 +17,87 @@
 			<Breadcrumb page_name={academics_program.title} />
 		</div>
 
-		<div class="prose px-4 py-10 text-base md:px-32">
-			{#if academics_program.flexible_content}
-				<FlexibleContent content={academics_program.flexible_content} />
-			{:else}
-				<p>Page is empty.</p>
-			{/if}
+		<div class="container mx-auto my-8 mb-5">
+			<Breadcrumb page_name={academics_program.title} />
 		</div>
 
-		<h1 class="mb-8 px-4 text-3xl font-bold text-gray-900 md:px-16">
-			Curriculum table for {academics_program.title}
-		</h1>
-		{#if academics_program.curriculum_table}
-			{#each [...new Set(academics_program.curriculum_table
-						.filter((item) => typeof item !== 'number')
-						.map((item) => item.year)
-						.sort((a, b) => a - b))] as year}
-				Year {year}
-				<br />
-				First semester
-				<br />
-				<DataTable
-					data={academics_program.curriculum_table
-						.filter((item) => typeof item !== 'number')
-						.filter((item) => item.year === year && item.semester === 'first')}
-				/>
-				<br />
-				Second semester
-				<br />
-				<DataTable
-					data={academics_program.curriculum_table
-						.filter((item) => typeof item !== 'number')
-						.filter((item) => item.year === year && item.semester === 'second')}
-				/>
-				<br />
-				Midyear
-				<br />
-				<DataTable
-					data={academics_program.curriculum_table
-						.filter((item) => typeof item !== 'number')
-						.filter((item) => item.year === year && item.semester === 'midyear')}
-				/>
-			{/each}
-		{/if}
+		<div class="flex justify-center px-4">
+			<div class="pb-16 md:pb-24 max-w-6xl w-full">
+				<div class="space-y-12 md:space-y-16">
+
+					<div>
+						<div class="flex space-x-2 pb-2 pt-5 text-xs font-medium">
+							<a class="" href="/">Home</a>
+							<p class="opacity-55">/</p>
+							<a class="" href="/academics">Academics</a>
+							<p class="opacity-55">/</p>
+							<p class="">idk :P</p>
+							<p class="opacity-55">/</p>
+							<p class="opacity-55">{academics_program.title}</p>
+						</div>
+						<div class="h-[1px] w-full bg-primary opacity-20"></div>
+					</div>
+
+					<div class="prose text-base">
+						{#if academics_program.flexible_content}
+							<FlexibleContent content={academics_program.flexible_content} />
+						{:else}
+							<p>Page is empty.</p>
+						{/if}
+					</div>
+
+					<div>
+						<div class="mb-6 md:mb-10">
+							<h1 class="text-2xl md:text-3xl leading-tight font-bold"> Curriculum Checklist </h1>
+							<p class="mt-2 ml-[2px] text-xs font-medium opacity-55"> Last Updated: March XX, 2025 </p>
+						</div>
+						
+						{#if academics_program.curriculum_table}
+							<div class="bg-white shadow-lg rounded-lg overflow-hidden p-4">
+								{#each [...new Set(academics_program.curriculum_table
+									.filter((item) => typeof item !== 'number')
+									.map((item) => item.year)
+									.sort((a, b) => a - b))] as year}
+								
+									<!-- Year Heading -->
+									<h2 class="text-2xl font-bold mt-8 border-b pb-2">Year {year}</h2>
+								
+									<!-- First Semester -->
+									<h3 class="font-semibold mt-4">First Semester</h3>
+									<div class="px-4">
+										<DataTable
+											data={academics_program.curriculum_table
+												.filter((item) => typeof item !== 'number')
+												.filter((item) => item.year === year && item.semester === 'first')}
+										/>
+									</div>
+								
+									<!-- Second Semester -->
+									<h3 class="font-semibold mt-4">Second Semester</h3>
+									<div class="px-4">
+										<DataTable
+											data={academics_program.curriculum_table
+												.filter((item) => typeof item !== 'number')
+												.filter((item) => item.year === year && item.semester === 'second')}
+										/>
+									</div>
+								
+									<!-- Midyear -->
+									<h3 class="font-semibold mt-4">Midyear</h3>
+									<div class="px-4">
+										<DataTable
+											data={academics_program.curriculum_table
+												.filter((item) => typeof item !== 'number')
+												.filter((item) => item.year === year && item.semester === 'midyear')}
+										/>
+									</div>
+								{/each}								
+							</div>
+						{/if}
+					</div>
+				</div>
+			</div>
+		</div>
 	{:else}
 		<p>Page not found</p>
 	{/if}
