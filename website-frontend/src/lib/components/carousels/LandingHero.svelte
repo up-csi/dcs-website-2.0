7ec520224a1857526db6b6a61c76696a6aa2f9a4
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-10 00:46:52 +0800

    feat: add progress bar to LandingHero

--- website-frontend/src/lib/components/carousels/LandingHero.svelte
+++ website-frontend/src/lib/components/carousels/LandingHero.svelte
@@ -5,14 +5,20 @@
 	import Autoplay from 'embla-carousel-autoplay';
 	import Fade from 'embla-carousel-fade';
 	import { Button } from '$lib/@shadcn-svelte/ui/button';
+	import { fly } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
 
-	const plugin = Autoplay({ delay: 4000, stopOnInteraction: true });
+	const delay = 6000;
+	const plugin = Autoplay({ delay, stopOnInteraction: true });
 
 	export let news;
 	let api: CarouselAPI;
 	let current = 0;
 	let count = 0;
-
+	let autoplayRunning = true;
+	let startTime = Date.now();
+	let elapsed = 0;
+	
 	$: if (api) {
 		count = api.scrollSnapList().length;
 		current = api.selectedScrollSnap() + 1;
@@ -21,17 +27,45 @@
 			current = api.selectedScrollSnap() + 1;
 		});
 	}
+
+	$: if (current) {
+		startTime = Date.now();
+		elapsed = 0;
+	}
+
+	function stopAutoplay() {
+		plugin.stop();
+		autoplayRunning = false;
+	}
+
+	function resetAutoplay() {
+		plugin.reset();
+		autoplayRunning = true;
+	}
+
+	function updateTime() {
+		if (autoplayRunning) {
+			elapsed = Date.now() - startTime;
+			requestAnimationFrame(updateTime);
+		}
+	}
+
+	function goToSlide(index: number) {
+		api.scrollTo(index);
+		progress = 1;
+	}
+
+	updateTime();
+	$: progress = Math.round((elapsed / delay) * 100);
 </script>
 
 <div>
 	<Carousel.Root
 		bind:api
 		plugins={[plugin, Fade()]}
-		on:mousenter={plugin.stop}
-		on:mouseleave={plugin.reset}
 	>
 		<Carousel.Content>
-			{#each news as news_item}
+			{#each news as news_item, index}
 				<Carousel.Item class="relative flex h-[90vh] flex-col items-center justify-end">
 					{#if news_item.background_image}
 						<img
@@ -41,28 +75,49 @@
 						/>
 					{/if}
 					<div
-						class="absolute flex h-full w-full flex-col justify-end bg-gradient-to-t from-black to-transparent"
+						class="absolute flex h-full w-full flex-col justify-end bg-gradient-to-t from-[#0000008e] to-transparent"
 					>
 						<div
-							class="container mx-auto flex flex-col items-center gap-y-5 pb-14 pt-72 text-center text-secondary-foreground md:items-start md:text-left"
+							class="container max-w-3xl mx-auto flex flex-col items-center gap-y-5 pb-14 pt-72 text-center text-secondary-foreground md:items-start md:text-left md:mx-0"
+							on:mouseenter={stopAutoplay}
+							on:mouseleave={resetAutoplay}
 						>
-							<h1 class="text-2xl font-bold md:text-4xl">{news_item.title}</h1>
-							<p class="text-xs font-semibold md:text-base">{news_item.summary}</p>
-							<small class="text-xs md:text-sm">
-								by {news_item.user_created.first_name}
-								{news_item.user_created.last_name}
-							</small>
-							<Button class="w-fit rounded-full px-5" href="/news/{news_item.slug}"
-								>Read Story</Button
-							>
-							<div class="flex gap-x-2 text-sm font-semibold text-secondary-foreground">
-								{#each [...Array(count).keys()] as index}
-									{#if index + 1 === current}
-										<div class="h-2 w-6 rounded-full bg-white"></div>
+							{#if index + 1 === current}
+								<div transition:fly={{ x: 40, duration: 1000, easing: cubicOut }}>
+									<h1 class="text-2xl font-bold md:text-4xl drop-shadow-lg">{news_item.title}</h1>
+								</div>
+
+								<div transition:fly={{ x: 40, duration: 900, easing: cubicOut }}>
+									<p class="text-xs font-medium md:text-base drop-shadow-lg line-clamp-3">{news_item.summary}</p>
+								</div>
+
+								<div transition:fly={{ x: 40, duration: 700, easing: cubicOut }}>
+									<small class="text-xs md:text-sm drop-shadow-lg">
+										by {news_item.user_created.first_name} {news_item.user_created.last_name}
+									</small>
+								</div>
+
+								<div transition:fly={{ x: 40, duration: 500, easing: cubicOut }}>
+									<Button class="w-fit rounded-full px-5" href="/news/{news_item.slug}">
+										Read Story
+									</Button>
+								</div>
+							{/if}
+
+							<!-- Pagination -->
+							<div class="flex gap-x-3 text-sm font-semibold text-secondary-foreground md:ml-1">
+								{#each [...Array(count).keys()] as i}
+									{#if i + 1 === current}
+										<div class="overflow-hidden relative h-2 w-8 rounded-full bg-white/40">
+											<div 
+												class="rounded-full absolute top-0 left-0 h-full bg-white transition-[width] duration-300 ease-linear"
+												style="width: {progress}%;"
+											></div>
+										</div>
 									{:else}
 										<button
-											class="h-2 w-2 rounded-full bg-gray-300"
-											on:click={() => api.scrollTo(index)}
+											class="h-2 w-2 rounded-full bg-white/40"
+											on:click={() => goToSlide(i)}
 										></button>
 									{/if}
 								{/each}
