a6da34e766af188c0a5090be968c229b12dd8c30
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-10 01:44:23 +0800

    feat: improve LandingHero styling

--- website-frontend/src/lib/components/carousels/LandingHero.svelte
+++ website-frontend/src/lib/components/carousels/LandingHero.svelte
@@ -66,7 +66,7 @@
 	>
 		<Carousel.Content>
 			{#each news as news_item, index}
-				<Carousel.Item class="relative flex h-[90vh] flex-col items-center justify-end">
+				<Carousel.Item class="relative flex h-[60vh] md:h-[90vh] flex-col items-center justify-end">
 					{#if news_item.background_image}
 						<img
 							src="{PUBLIC_APIURL}/assets/{news_item.background_image}"
@@ -75,37 +75,55 @@
 						/>
 					{/if}
 					<div
-						class="absolute flex h-full w-full flex-col justify-end bg-gradient-to-t from-[#0000008e] to-transparent"
+						class="absolute flex h-full w-full flex-col justify-end bg-gradient-to-t from-[#0000009e] to-transparent"
 					>
 						<div
-							class="container max-w-3xl mx-auto flex flex-col items-center gap-y-5 pb-14 pt-72 text-center text-secondary-foreground md:items-start md:text-left md:mx-0"
+							class="container max-w-3xl mx-auto flex flex-col items-center gap-y-5 pb-9 md:pb-14 pt-72 text-center text-secondary-foreground md:items-start md:text-left md:mx-12"
 							on:mouseenter={stopAutoplay}
 							on:mouseleave={resetAutoplay}
 						>
 							{#if index + 1 === current}
-								<div transition:fly={{ x: 40, duration: 1000, easing: cubicOut }}>
-									<h1 class="text-2xl font-bold md:text-4xl drop-shadow-lg">{news_item.title}</h1>
-								</div>
+								<div 
+									in:fly={{ x: 40, duration: 1000, easing: cubicOut }} 
+									out:fly={{ x: -40, duration: 1000, easing: cubicOut }}
+								>
+									<h1 class="text-2xl leading-tight md:leading-tight font-bold md:text-4xl drop-shadow-lg">{news_item.title}</h1>
+					   			</div>					   
 
-								<div transition:fly={{ x: 40, duration: 900, easing: cubicOut }}>
+								<div 
+									class="hidden md:block"
+								   	in:fly={{ x: 40, duration: 900, easing: cubicOut }} 
+									out:fly={{ x: -40, duration: 900 }}
+							   	>
 									<p class="text-xs font-medium md:text-base drop-shadow-lg line-clamp-3">{news_item.summary}</p>
 								</div>
 
-								<div transition:fly={{ x: 40, duration: 700, easing: cubicOut }}>
-									<small class="text-xs md:text-sm drop-shadow-lg">
+								<div 
+									class="block md:hidden"
+								   	in:fly={{ x: 40, duration: 700, easing: cubicOut }} 
+									out:fly={{ x: -40, duration: 700 }}
+							   	>
+								   <p class="text-xs md:text-sm text-white font-medium opacity-70 drop-shadow-lg">
 										by {news_item.user_created.first_name} {news_item.user_created.last_name}
-									</small>
+									</p>
 								</div>
 
-								<div transition:fly={{ x: 40, duration: 500, easing: cubicOut }}>
+								<div
+									class="flex items-center space-x-6" 
+								   	in:fly={{ x: 40, duration: 500, easing: cubicOut }} 
+									out:fly={{ x: -40, duration: 500 }}
+							   	>
 									<Button class="w-fit rounded-full px-5" href="/news/{news_item.slug}">
 										Read Story
 									</Button>
+									<p class="hidden md:block text-xs md:text-sm text-white font-medium opacity-70 drop-shadow-lg">
+										by {news_item.user_created.first_name} {news_item.user_created.last_name}
+									</p>
 								</div>
 							{/if}
 
 							<!-- Pagination -->
-							<div class="flex gap-x-3 text-sm font-semibold text-secondary-foreground md:ml-1">
+							<div class="flex gap-x-3 text-sm font-semibold text-secondary-foreground md:ml-[6px] md:mt-4">
 								{#each [...Array(count).keys()] as i}
 									{#if i + 1 === current}
 										<div class="overflow-hidden relative h-2 w-8 rounded-full bg-white/40">
