84c44483310a8d94103e07863d5b35a5cfd6ccd3
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-09 20:57:07 +0800

    feat: add background-dark

--- website-frontend/tailwind.config.ts
+++ website-frontend/tailwind.config.ts
@@ -19,7 +19,10 @@ const config: Config = {
 				border: 'hsl(var(--border) / <alpha-value>)',
 				input: 'hsl(var(--input) / <alpha-value>)',
 				ring: 'hsl(var(--ring) / <alpha-value>)',
-				background: 'hsl(var(--background) / <alpha-value>)',
+				background: {
+					DEFAULT: 'hsl(var(--background) / <alpha-value>)',
+					dark: 'hsl(var(--background-dark) / <alpha-value>)'
+				},
 				foreground: 'hsl(var(--foreground) / <alpha-value>)',
 				primary: {
 					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
