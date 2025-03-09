6ecc12df017de4a6bda33d266c42eb34a40591e5
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2025-03-09 16:01:27 +0800

    chore: run linter

--- website-frontend/src/routes/students/organizations/+page.svelte
+++ website-frontend/src/routes/students/organizations/+page.svelte
@@ -4,7 +4,6 @@
 	import Breadcrumb from '$lib/components/breadcrumbs/PageBreadcrumb.svelte';
 	import FlexibleContent from '$lib/components/flexible_content/FlexibleContent.svelte';
 	import OrganizationItem from '$lib/components/list_items/OrganizationItem.svelte';
-	import { PUBLIC_APIURL } from '$env/static/public';
 	export let data;
 
 	$: ({ students_organizations, students_organizations_overview } = data);
@@ -13,10 +12,7 @@
 </script>
 
 <body>
-	<Banner
-		{title}
-		background_image={students_organizations_overview.background_image ?? ''}
-	/>
+	<Banner {title} background_image={students_organizations_overview.background_image ?? ''} />
 
 	<div class="mx-20 mb-36">
 		<div class="my-5">
@@ -24,12 +20,12 @@
 		</div>
 
 		{#if students_organizations_overview.flexible_content}
-            <div class="w-full px-40 mb-10">
-                <FlexibleContent content={students_organizations_overview.flexible_content} />
-            </div>
-        {/if}
-		
-		<p class="font-bold text-2xl">CS Network</p>
+			<div class="mb-10 w-full px-40">
+				<FlexibleContent content={students_organizations_overview.flexible_content} />
+			</div>
+		{/if}
+
+		<p class="text-2xl font-bold">CS Network</p>
 
 		{#each students_organizations as organization}
 			<OrganizationItem {organization} />
