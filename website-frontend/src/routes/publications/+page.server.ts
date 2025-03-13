4e11ba094ad0816013d893c1321543961dfd22d6
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-13 10:18:12 +0800

    feat: add server logic for publications year filter

--- website-frontend/src/routes/publications/+page.server.ts
+++ website-frontend/src/routes/publications/+page.server.ts
@@ -6,10 +6,29 @@ export async function load({ fetch, url }) {
 	const directus = getDirectusInstance(fetch);
 
 	const filters = {
-		year: url.searchParams.getAll('year'),
+		years: url.searchParams.getAll('year'),
 		laboratories: url.searchParams.getAll('laboratory'),
 		tags: url.searchParams.getAll('tags')
 	};
+
+	const years_filters = await directus
+		.request(
+			readItems('publications', {
+				fields: ['publish_date'],
+				sort: ['-publish_date']
+			})
+		)
+		.then((res) => {
+			const years = res
+				.map((pub) => {
+					if (pub.publish_date) {
+						return new Date(pub.publish_date).getFullYear();
+					}
+				})
+				.filter((year) => year !== undefined);
+
+			return years.filter((item, index) => years.indexOf(item) === index);
+		});
 
 	const laboratories_filters = await directus
 		.request(
@@ -33,6 +52,7 @@ export async function load({ fetch, url }) {
 		.request(
 			readItems('publications', {
 				filter: {
+					'year(publish_date)': { _in: filters.years.length !== 0 ? filters.years : undefined },
 					laboratory: {
 						name: { _in: filters.laboratories.length !== 0 ? filters.laboratories : undefined }
 					},
@@ -68,5 +88,5 @@ export async function load({ fetch, url }) {
 				)
 		);
 
-	return { publications, laboratories_filters, tags_filters };
+	return { publications, years_filters, laboratories_filters, tags_filters };
 }
