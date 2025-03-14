57f5b75059f137eda5f5da1b8e8aeb5363c2de6a
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-15 04:02:11 +0800

    chore: adjust footer paddings further

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	import LandingHero from '$lib/components/carousels/LandingHero.svelte';
 	import FeaturedEventCard from '$lib/components/cards/FeaturedEventCard.svelte';
 	import NewsCard from '$lib/components/cards/NewsCard.svelte';
-	import { ArrowRight } from 'lucide-svelte';
+	import { ChevronRight } from 'lucide-svelte';
 
 	export let data;
 
@@ -14,46 +14,55 @@
 
 <LandingHero {news} />
 
-<div class="container mx-auto my-8 flex flex-col justify-center gap-y-5">
-	<div class="flex justify-between">
-		<h2 class="text-xl font-bold md:text-2xl">Recent News</h2>
-		<Button href="#more-news" variant="outline" class="flex gap-x-2 rounded-full">
-			<p>View all</p>
-			<ArrowRight class="size-4" />
-		</Button>
+<div class="flex justify-center px-4">
+	<div class="w-full max-w-7xl pt-10 md:pt-20 pb-16 md:pb-24 space-y-16 md:space-y-24">
+
+		<div>
+			<div class="heading-padding flex justify-between items-center">
+				<h2 class="heading-text"> Recent News </h2>
+				<Button href="#more-news" variant="outline" class="flex gap-x-1 rounded-full text-[0.8rem] h-auto">
+					<p>View all</p>
+					<ChevronRight class="size-4" />
+				</Button>
+			</div>
+			<Carousel.Root opts={{ align: 'start', dragFree: true }}>
+				<Carousel.Content>
+					{#each news as news_item}
+						<Carousel.Item class="-mr-10 basis-full pr-10 md:-mr-5 md:basis-1/4 md:pr-5">
+							<NewsCard {news_item} />
+						</Carousel.Item>
+					{/each}
+				</Carousel.Content>
+				<Carousel.Next />
+			</Carousel.Root>
+		</div>
+
+		<div>
+			<div class="heading-padding flex justify-between items-center">
+				<h2 class="heading-text"> Recent Events </h2>
+				<Button href="#more-news" variant="outline" class="flex gap-x-1 rounded-full text-[0.8rem] h-auto">
+					<p>View all</p>
+					<ChevronRight class="size-4" />
+				</Button>
+			</div>
+			<Carousel.Root opts={{ align: 'start', dragFree: true }}>
+				<Carousel.Content>
+					{#each events as event}
+						<Carousel.Item class="-mr-10 basis-full pr-10 md:-mr-5 md:basis-1/4 md:pr-5">
+							<FeaturedEventCard {event} />
+						</Carousel.Item>
+					{/each}
+				</Carousel.Content>
+				<Carousel.Next />
+			</Carousel.Root>
+		</div>
+
 	</div>
-	<Carousel.Root opts={{ align: 'start', dragFree: true }}>
-		<Carousel.Content>
-			{#each news as news_item}
-				<Carousel.Item class="-mr-10 basis-full pr-10 md:-mr-5 md:basis-1/4 md:pr-5">
-					<NewsCard {news_item} />
-				</Carousel.Item>
-			{/each}
-		</Carousel.Content>
-		<Carousel.Next />
-	</Carousel.Root>
-	<div class="flex justify-between">
-		<h2 class="text-xl font-bold md:text-2xl">Recent Events</h2>
-		<Button href="/events" variant="outline" class="flex gap-x-2 rounded-full">
-			<p>View all</p>
-			<ArrowRight class="size-4" />
-		</Button>
-	</div>
-	<Carousel.Root opts={{ align: 'start', dragFree: true }}>
-		<Carousel.Content>
-			{#each events as event}
-				<Carousel.Item class="-mr-10 basis-full pr-10 md:-mr-5 md:basis-1/4 md:pr-5">
-					<FeaturedEventCard {event} />
-				</Carousel.Item>
-			{/each}
-		</Carousel.Content>
-		<Carousel.Next />
-	</Carousel.Root>
 </div>
 
-<div id="more-news" class="bg-background-dark py-24 text-primary-foreground">
-	<div class="container flex h-full flex-col justify-center gap-y-8">
-		<h2 class="text-center text-xl font-bold md:text-2xl">More News from UPD DCS</h2>
+<div id="more-news" class="flex justify-center px-4 bg-background-dark text-primary-foreground">
+	<div class="w-full max-w-7xl py-16 md:py-24">
+		<h2 class="heading-text heading-padding text-center">More News from UPD DCS</h2>
 		<div class="flex flex-col gap-5 md:grid md:grid-cols-4">
 			{#each news as news_item}
 				<NewsCard onDark {news_item} />
