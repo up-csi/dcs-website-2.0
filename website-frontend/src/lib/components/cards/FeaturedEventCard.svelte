da2aa3a09fc3f1a62f8566a4cddc7a9547e93bb2
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-15 14:36:46 +0800

    feat: add CardCarousel

--- website-frontend/src/lib/components/cards/FeaturedEventCard.svelte
+++ website-frontend/src/lib/components/cards/FeaturedEventCard.svelte
@@ -2,42 +2,37 @@
 	import { PUBLIC_APIURL } from '$env/static/public';
 	import { Event } from '$lib/models/event';
 	import { Calendar, MapPin } from 'lucide-svelte';
-	export let event: Event;
+	export let item: Event;
 </script>
 
-<a href="/events/{event.slug}" data-sveltekit-reload>
+<a href="/events/{item.slug}" data-sveltekit-reload>
 	<div
-		class="relative flex h-[25rem] flex-col overflow-hidden rounded-lg bg-white text-gray-800 shadow-xl"
+		class="relative flex h-[25rem] flex-col rounded-lg bg-white text-gray-800 shadow-xl"
 	>
 		<div class="inset-0 h-24 flex-grow rounded-t-lg bg-gray-300 md:h-44">
-			{#if event.hero_image}
+			{#if item.hero_image}
 				<img
 					class="h-full w-full rounded-t-lg object-cover"
-					src="{PUBLIC_APIURL}/assets/{event.hero_image}"
+					src="{PUBLIC_APIURL}/assets/{item.hero_image}"
 					alt="Background"
 				/>
 			{/if}
 		</div>
 
-		<div>
-			<div class="absolute -left-3 -mt-2 h-6 w-6 rounded-full bg-slate-100 shadow-inner"></div>
-			<div class="absolute -right-3 -mt-2 h-6 w-6 rounded-full bg-slate-100 shadow-inner"></div>
-		</div>
-
 		<div class="space-y-2 p-4">
 			<div class="line-clamp-3 text-lg font-bold">
-				<h1>{event.event_headline}</h1>
+				<h1>{item.event_headline}</h1>
 			</div>
 			<div class="flex items-center space-x-2 text-sm font-medium">
 				<Calendar class="h-4 w-4" />
 				<p>
-					{new Date(event.start_date).toLocaleDateString('en-US', {
+					{new Date(item.start_date).toLocaleDateString('en-US', {
 						month: 'long',
 						day: 'numeric',
 						year: 'numeric'
 					})}
-					{#if event.end_date}
-						- {new Date(event.end_date).toLocaleDateString('en-US', {
+					{#if item.end_date}
+						- {new Date(item.end_date).toLocaleDateString('en-US', {
 							month: 'long',
 							day: 'numeric',
 							year: 'numeric'
@@ -45,17 +40,17 @@
 					{/if}
 				</p>
 			</div>
-			{#if event.display_location}
+			{#if item.display_location}
 				<div class="flex items-center space-x-2 text-sm text-gray-500">
 					<MapPin class="h-4 w-4" />
-					<p>{event.display_location}</p>
+					<p>{item.display_location}</p>
 				</div>
 			{/if}
-			{#if event.event_tags}
+			{#if item.event_tags}
 				<div class="py-1 text-xs font-bold text-gray-600">
-					{#each event.event_tags as tag, index}
+					{#each item.event_tags as tag, index}
 						{#if typeof tag !== 'string' && typeof tag.events_tags_id !== 'string'}
-							<p>{tag.events_tags_id.name}{index < event.event_tags.length - 1 ? ',' : ''}</p>
+							<p>{tag.events_tags_id.name}{index < item.event_tags.length - 1 ? ',' : ''}</p>
 						{/if}
 					{/each}
 				</div>
--- website-frontend/src/lib/components/cards/NewsCard.svelte
+++ website-frontend/src/lib/components/cards/NewsCard.svelte
@@ -3,21 +3,21 @@
 	import { PUBLIC_APIURL } from '$env/static/public';
 	import { NewsItem } from '$lib/models/news';
 	import { ScrollText } from 'lucide-svelte';
-	export let news_item: NewsItem;
+	export let item: NewsItem;
 	export let onDark = false;
 </script>
 
-<a href="/news/{news_item.slug}" data-sveltekit-reload>
+<a href="/news/{item.slug}" data-sveltekit-reload>
 	<Card.Root
 		class={onDark
 			? 'flex h-[25rem] flex-col justify-end overflow-clip rounded-lg border-primary'
 			: 'flex h-[25rem] flex-col justify-end overflow-clip rounded-lg'}
 	>
 		<div class="flex min-h-0 flex-1 justify-center">
-			{#if news_item.background_image}
+			{#if item.background_image}
 				<img
-					src="{PUBLIC_APIURL}/assets/{news_item.background_image}"
-					alt={news_item.title}
+					src="{PUBLIC_APIURL}/assets/{item.background_image}"
+					alt={item.title}
 					class="w-full object-cover"
 				/>
 			{:else}
@@ -27,17 +27,17 @@
 			{/if}
 		</div>
 		<Card.Header>
-			<Card.Title class="line-clamp-3">{news_item.title}</Card.Title>
+			<Card.Title class="line-clamp-3">{item.title}</Card.Title>
 		</Card.Header>
 		<Card.Content>
-			<Card.Description class="line-clamp-2">{news_item.summary}</Card.Description>
+			<Card.Description class="line-clamp-2">{item.summary}</Card.Description>
 		</Card.Content>
 		<Card.Footer class="flex justify-between">
 			<small class="text-xs text-gray-500"
-				>by {news_item.user_created.first_name} {news_item.user_created.last_name}</small
+				>by {item.user_created.first_name} {item.user_created.last_name}</small
 			>
 			<small class="text-xs text-gray-500">
-				{new Date(news_item.date_created).toLocaleDateString('en-US', {
+				{new Date(item.date_created).toLocaleDateString('en-US', {
 					month: 'long',
 					day: 'numeric',
 					year: 'numeric'
