644dd8794cad3d8f4c39d2bdad14c4c5df658c63
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-15 18:24:17 +0800

    feat: style cards & CardCarousel

--- website-frontend/src/lib/components/cards/NewsCard.svelte
+++ website-frontend/src/lib/components/cards/NewsCard.svelte
@@ -1,48 +1,42 @@
 <script lang="ts">
-	import * as Card from '$lib/@shadcn-svelte/ui/card';
 	import { PUBLIC_APIURL } from '$env/static/public';
 	import { NewsItem } from '$lib/models/news';
-	import { ScrollText } from 'lucide-svelte';
+	import { Image } from 'lucide-svelte';
 	export let item: NewsItem;
-	export let onDark = false;
 </script>
 
 <a href="/news/{item.slug}" data-sveltekit-reload>
-	<Card.Root
-		class={onDark
-			? 'flex h-[25rem] flex-col justify-end overflow-clip rounded-lg border-primary'
-			: 'flex h-[25rem] flex-col justify-end overflow-clip rounded-lg'}
+	<div
+		class="relative flex h-[25rem] flex-col rounded-lg bg-white text-gray-800 shadow-md mb-2 group"
 	>
-		<div class="flex min-h-0 flex-1 justify-center">
-			{#if item.background_image}
+	<div class="inset-0 h-24 flex-grow rounded-t-lg md:h-44 overflow-hidden
+		{item.background_image ? '' : 'bg-gradient-to-b from-[#D1D8DD] to-[#66708076] flex items-center justify-center'}"
+	>
+		{#if item.background_image}
+			<div class="h-full w-full">
 				<img
-					src="{PUBLIC_APIURL}/assets/{item.background_image}"
-					alt={item.title}
-					class="w-full object-cover"
+				  class="h-full w-full rounded-t-lg object-cover transition-transform duration-300 ease-out group-hover:scale-105"
+				  src="{PUBLIC_APIURL}/assets/{item.background_image}"
+				  alt="Background"
 				/>
-			{:else}
-				<div class="flex w-full items-center justify-center bg-muted">
-					<ScrollText class="w-20 bg-muted text-muted-foreground" />
-				</div>
-			{/if}
-		</div>
-		<Card.Header>
-			<Card.Title class="line-clamp-3">{item.title}</Card.Title>
-		</Card.Header>
-		<Card.Content>
-			<Card.Description class="line-clamp-2">{item.summary}</Card.Description>
-		</Card.Content>
-		<Card.Footer class="flex justify-between">
-			<small class="text-xs text-gray-500"
-				>by {item.user_created.first_name} {item.user_created.last_name}</small
-			>
-			<small class="text-xs text-gray-500">
-				{new Date(item.date_created).toLocaleDateString('en-US', {
+		  	</div>
+		{:else}
+			<Image class="h-8 w-8 text-white opacity-70" />
+		{/if}
+	</div>
+
+		<div class="space-y-4 p-4">
+			<h1 class="line-clamp-3 text-[17px] font-bold leading-tight">{item.title}</h1>
+			<p class="line-clamp-2 text-[13px] opacity-60 leading-tight">{item.summary}</p>
+			<div class="flex justify-between">
+				<p class="uppercase py-1 text-[11px] font-bold opacity-60 flex flex-wrap gap-1 leading-none"> tag </p>
+				<p class="text-[11px] font-medium opacity-60">
+					{new Date(item.date_created).toLocaleDateString('en-GB', {
 					month: 'long',
 					day: 'numeric',
 					year: 'numeric'
-				})}
-			</small>
-		</Card.Footer>
-	</Card.Root>
+				})}</p>
+			</div>
+		</div>
+	</div>
 </a>
