44f078130fb2ec13b0f168464d10297cb2953eb0
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-15 11:10:34 +0800

    fix: fix footer padding based on page

--- website-frontend/src/routes/+page.svelte
+++ website-frontend/src/routes/+page.svelte
@@ -15,9 +15,9 @@
 <LandingHero {news} />
 
 <div class="flex justify-center px-4">
-	<div class="w-full max-w-7xl pt-10 md:pt-20 pb-16 md:pb-24 space-y-16 md:space-y-24">
+	<div class="w-full md:px-14 pt-10 md:pt-20 pb-16 md:pb-24 space-y-16 md:space-y-24">
 
-		<div>
+		<div class="overflow-visible">
 			<div class="heading-padding flex justify-between items-center">
 				<h2 class="heading-text"> Recent News </h2>
 				<Button href="#more-news" variant="outline" class="flex gap-x-1 rounded-full text-[0.8rem] h-auto">
@@ -26,13 +26,14 @@
 				</Button>
 			</div>
 			<Carousel.Root opts={{ align: 'start', dragFree: true }}>
-				<Carousel.Content>
+				<Carousel.Content class="">
 					{#each news as news_item}
-						<Carousel.Item class="-mr-10 basis-full pr-10 md:-mr-5 md:basis-1/4 md:pr-5">
+						<Carousel.Item class="basis-full md:basis-1/4">
 							<NewsCard {news_item} />
 						</Carousel.Item>
 					{/each}
 				</Carousel.Content>
+				<Carousel.Previous class="absolute left-0"/>
 				<Carousel.Next />
 			</Carousel.Root>
 		</div>
@@ -45,15 +46,22 @@
 					<ChevronRight class="size-4" />
 				</Button>
 			</div>
-			<Carousel.Root opts={{ align: 'start', dragFree: true }}>
-				<Carousel.Content>
+			<Carousel.Root opts={{ align: 'start', dragFree: true }} class="w-screen -ml-[50vw] left-1/2 relative">
+				<Carousel.Content class="-ml-[90%] -mr-[80%] md:-ml-[18%] md:-mr-[17%]">
+					<Carousel.Item class="basis-1/3 md:basis-1/6">
+						<div class="h-full"/>
+					</Carousel.Item>
 					{#each events as event}
-						<Carousel.Item class="-mr-10 basis-full pr-10 md:-mr-5 md:basis-1/4 md:pr-5">
+						<Carousel.Item class="basis-1/3 md:basis-1/6">
 							<FeaturedEventCard {event} />
 						</Carousel.Item>
 					{/each}
+					<Carousel.Item class="basis-1/3 md:basis-1/6">
+						<div class="h-full"/>
+					</Carousel.Item>
 				</Carousel.Content>
-				<Carousel.Next />
+				<Carousel.Previous class="absolute left-0 rounded-md h-16 w-20" />
+				<Carousel.Next class="absolute right-0 rounded-md"/>
 			</Carousel.Root>
 		</div>
 
