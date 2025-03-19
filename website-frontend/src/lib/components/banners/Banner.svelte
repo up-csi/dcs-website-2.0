94399897f4b265a413664931bf145a1792efd5b7
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-20 07:53:14 +0800

    feat: make banners more uniform

--- website-frontend/src/lib/components/banners/Banner.svelte
+++ website-frontend/src/lib/components/banners/Banner.svelte
@@ -5,50 +5,28 @@
 	export let title: string;
 	export let background_image: string = '';
 	export let flexible_content: string = '';
-	export let from_bottom: string = '';
-
-	let text_bottom = 'bottom-9';
-	$: if (from_bottom) {
-		text_bottom = from_bottom;
-	} else if (flexible_content) {
-		text_bottom = 'bottom-24';
-	}
-
-	let height = `h-[40vh]`;
-	$: if (flexible_content) {
-		height = 'h-[45vh]';
-	}
-
-	let max_height = 'md:h-[50vh]';
-	$: if (flexible_content) {
-		max_height = 'md:h-[60vh]';
-	}
-
-	let font_size = 'md:text-5xl';
-	$: if (flexible_content) {
-		font_size = 'md-text-4xl';
-	}
 
 	let background =
-		'background-image: linear-gradient(to top, hsl(var(--primary)), hsl(var(--background)))';
+		'background-image: linear-gradient(to top, hsl(var(--primary)) -20%, hsl(var(--secondary)) 55%)';
 	$: if (background_image) {
-		background = `background-image: linear-gradient(to top, hsl(var(--primary)), transparent), url('${PUBLIC_APIURL}/assets/${background_image}')`;
+		background = `background-image: linear-gradient(to top, hsl(var(--primary)) -20%, transparent 55%), url('${PUBLIC_APIURL}/assets/${background_image}')`;
 	}
 
 	$: deslugify_title = deslugify(title);
 </script>
 
 <div class="relative z-0">
-	<div class="{height} bg-cover bg-center {max_height}" style={background}></div>
+	<div class="h-[45vh] bg-cover bg-center md:h-[60vh]" style={background}></div>
 
 	<div
-		class="absolute {text_bottom} left-1/2
-        -translate-x-1/2 transform text-center text-primary-foreground
-        md:left-0 md:max-w-[60vw] md:translate-x-0 md:px-32 md:text-start"
+		class="absolute bottom-11 w-full text-white px-4 text-center
+		md:flex md:justify-center"
 	>
-		<h1 class="text-3xl font-bold {font_size}">{deslugify_title}</h1>
-		{#if flexible_content}
-			<h4 class="hidden text-muted md:mt-4 md:block">{@html flexible_content}</h4>
-		{/if}
+		<div class="w-full max-w-6xl text-center md:text-start">
+			<h1 class="text-3xl font-bold leading-none md:text-5xl md:max-w-[50vw]">{deslugify_title}</h1>
+			{#if flexible_content}
+				<h4 class="hidden md:mt-4 md:block md:max-w-[50vw]">{@html flexible_content}</h4>
+			{/if}
+		</div>
 	</div>
 </div>
