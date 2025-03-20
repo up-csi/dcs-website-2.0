63a36676304426b5924df2e3e936dcaa11df8e8f
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-20 18:05:43 +0800

    feat: add division per rank in people pages

--- website-frontend/src/routes/people/[slug]/+page.svelte
+++ website-frontend/src/routes/people/[slug]/+page.svelte
@@ -4,34 +4,48 @@
 	import CardPanel from '$lib/components/panel/CardPanel.svelte';
 	import PeopleCard from '$lib/components/cards/PeopleCard.svelte';
 	import Breadcrumb from '$lib/components/breadcrumbs/PageBreadcrumb.svelte';
+	import { Person } from '$lib/models/people';
 
 	export let data;
 
 	$: ({ category, people } = data);
+
+	$: positions = Array.from(new Set(people.map((person: Person) => person.position).filter(Boolean)));
+	$: peopleByPosition = positions.map((position) => ({
+	    position,
+	    people: people.filter((person: Person) => person.position === position)
+	}));
 </script>
 
 <body>
 	{#if category}
-		<div class="relative z-0">
-			<Banner
-				title={category.title}
-				background_image={category.background_image ?? ''}
-				flexible_content={category.flexible_content}
-			/>
-		</div>
+		<Banner
+			title={category.title}
+			background_image={category.background_image ?? ''}
+			flexible_content={category.flexible_content}
+		/>
 
-		<div class="mx-auto w-[94vw] md:w-[80vw]">
-			<div class="mt-5">
-				<Breadcrumb />
-			</div>
+		<div class="flex justify-center px-4">
+			<div class="content-padding">
 
-			<CardPanel>
-				{#each people as person}
-					<a href="/people/{category.title}/{person.username}">
-						<PeopleCard {person} laboratory={person.affiliations?.[0]?.laboratories_id?.name} />
-					</a>
+				<div class="pt-5">
+					<Breadcrumb />
+				</div>
+
+				{#each peopleByPosition as { position, people } }
+					<div>
+						<p class="heading-text">{position}s</p>
+						<CardPanel>
+							{#each people as person (person.username)}
+								<a href="/people/{person.category}/{person.username}">
+									<PeopleCard {person} laboratory={person.affiliations?.[0]?.laboratories_id?.name} />
+								</a>
+							{/each}
+						</CardPanel>
+					</div>
 				{/each}
-			</CardPanel>
+
+			</div>
 		</div>
 	{:else}
 		<p>People category not found</p>
