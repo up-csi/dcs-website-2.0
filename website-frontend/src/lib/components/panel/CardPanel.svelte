4a152aac8012e82b80a337cdc8f37a3e1984397b
Clarisse Bianca Bucu <isaybucu@gmail.com>
2025-03-20 18:05:07 +0800

    fix: remove padding from CardPanel component

--- website-frontend/src/lib/components/panel/CardPanel.svelte
+++ website-frontend/src/lib/components/panel/CardPanel.svelte
@@ -5,7 +5,7 @@
 <div
 	class="mx-auto my-3 grid
     max-w-[94vw] justify-center gap-2
-    pb-20 md:my-8
+    md:my-8
     md:max-w-[80vw] lg:gap-3"
 	style="grid-template-columns: repeat(auto-fit, {width}px);"
 >
