3263df6661b59b9a28a5c4eb34e6d173b89588db
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-21 09:39:02 +0800

    feat: add mobile text styling for courses table

--- website-frontend/src/lib/components/table/CoursesTable.svelte
+++ website-frontend/src/lib/components/table/CoursesTable.svelte
@@ -11,22 +11,22 @@
 			<Table.Header class="bg-gray-100">
 				<Table.Row>
 					<Table.Head
-						class="w-1/12 min-w-28 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+						class="w-1/12 min-w-20 bg-gray-100 px-4 py-2 text-left text-xs font-semibold text-gray-700 md:min-w-28 md:text-sm"
 					>
 						Code
 					</Table.Head>
 					<Table.Head
-						class="w-3/12 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+						class="w-3/12 bg-gray-100 px-4 py-2 text-left text-xs font-semibold text-gray-700 md:text-sm"
 					>
 						Title
 					</Table.Head>
 					<Table.Head
-						class="w-1/12 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+						class="w-1/12 bg-gray-100 px-4 py-2 text-left text-xs font-semibold text-gray-700 md:text-sm"
 					>
 						Units
 					</Table.Head>
 					<Table.Head
-						class="w-7/12 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+						class="w-7/12 bg-gray-100 px-4 py-2 text-left text-xs font-semibold text-gray-700 md:text-sm"
 					>
 						Description
 					</Table.Head>
@@ -35,16 +35,18 @@
 			<Table.Body>
 				{#each academics_courses as { course_code, course_title, course_description, course_units }}
 					<Table.Row>
-						<Table.Cell class="w-1/12 min-w-28 bg-white text-gray-600">
+						<Table.Cell
+							class="w-1/12 min-w-20 bg-white text-xs text-gray-600 md:min-w-28 md:text-sm"
+						>
 							{course_code}
 						</Table.Cell>
-						<Table.Cell class="w-3/12 bg-white text-gray-600">
+						<Table.Cell class="w-3/12 bg-white text-xs text-gray-600 md:text-sm">
 							{course_title}
 						</Table.Cell>
-						<Table.Cell class="w-1/12 bg-white text-gray-600">
+						<Table.Cell class="w-1/12 bg-white text-xs text-gray-600 md:text-sm">
 							{course_units}
 						</Table.Cell>
-						<Table.Cell class="w-7/12 bg-white text-gray-600">
+						<Table.Cell class="w-7/12 bg-white text-xs text-gray-600 md:text-sm">
 							{#if course_description}
 								{course_description}
 							{/if}
