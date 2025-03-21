e24f4bb3eae857b5334745870c0102255b42aa19
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-03-21 09:34:58 +0800

    feat: ensure clean spacing for course table elements

--- website-frontend/src/lib/components/table/CoursesTable.svelte
+++ website-frontend/src/lib/components/table/CoursesTable.svelte
@@ -10,26 +10,41 @@
 		<Table.Root class="overflow-hidden rounded-lg">
 			<Table.Header class="bg-gray-100">
 				<Table.Row>
-					{#each ['Code', 'Title', 'Units', 'Description'] as heading}
-						<Table.Head class="bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700">
-							{heading}
-						</Table.Head>
-					{/each}
+					<Table.Head
+						class="w-1/12 min-w-28 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+					>
+						Code
+					</Table.Head>
+					<Table.Head
+						class="w-3/12 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+					>
+						Title
+					</Table.Head>
+					<Table.Head
+						class="w-1/12 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+					>
+						Units
+					</Table.Head>
+					<Table.Head
+						class="w-7/12 bg-gray-100 px-4 py-2 text-left text-sm font-semibold text-gray-700"
+					>
+						Description
+					</Table.Head>
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
 				{#each academics_courses as { course_code, course_title, course_description, course_units }}
 					<Table.Row>
-						<Table.Cell class="bg-white text-gray-600">
+						<Table.Cell class="w-1/12 min-w-28 bg-white text-gray-600">
 							{course_code}
 						</Table.Cell>
-						<Table.Cell class="bg-white text-gray-600">
+						<Table.Cell class="w-3/12 bg-white text-gray-600">
 							{course_title}
 						</Table.Cell>
-						<Table.Cell class="bg-white text-gray-600">
+						<Table.Cell class="w-1/12 bg-white text-gray-600">
 							{course_units}
 						</Table.Cell>
-						<Table.Cell class="bg-white text-gray-600">
+						<Table.Cell class="w-7/12 bg-white text-gray-600">
 							{#if course_description}
 								{course_description}
 							{/if}
