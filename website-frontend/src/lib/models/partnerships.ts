8327f07f90aab443e4c406aa7a39727a82c42c86
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-04-08 17:21:59 +0800

    fix: set partnerships to also export its type

--- website-frontend/src/lib/models/partnerships.ts
+++ website-frontend/src/lib/models/partnerships.ts
@@ -1,7 +1,9 @@
-import { nullable, object, optional, string } from "valibot";
+import { nullable, object, optional, string, type InferOutput } from "valibot";
 
 export const Partnerships = object({
     name: string(),
     display_image: optional(nullable(string())),
     description: string(),
 })
+
+export type Partnerships = InferOutput<typeof Partnerships>;
