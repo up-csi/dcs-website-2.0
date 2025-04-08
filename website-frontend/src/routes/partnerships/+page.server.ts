0e9d9472180ff6542f9851da1f036f69a4b164fa
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-04-08 17:25:37 +0800

    feat: use new partnerships data in server load

--- website-frontend/src/routes/partnerships/+page.server.ts
+++ website-frontend/src/routes/partnerships/+page.server.ts
@@ -1,10 +1,15 @@
 /** @type {import('./$types').PageServerLoad} */
 import getDirectusInstance from '$lib/directus.js';
-import type { Partners } from '$lib/types/partners';
 import { readItems } from '@directus/sdk';
 
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	
-	const partners = await directus.request();
+	const content = await directus.request(readItems('partnerships_overview'));
+	const partnerships = await directus.request(readItems('partnerships'));
+
+	return {
+		content,
+		partnerships
+	}
 }
