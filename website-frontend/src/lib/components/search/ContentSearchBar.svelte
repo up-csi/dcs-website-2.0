bb3eaeae102d13381ce804a6c2213633059e3791
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-05-16 17:40:09 +0800

    feat: add in-content search bar component

---
+++ website-frontend/src/lib/components/search/ContentSearchBar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Input } from '$lib/@shadcn-svelte/ui/input';
+
+	export let placeholder: string;
+</script>
+
+<form class="flex w-full max-w-sm items-center space-x-2">
+	<Input name="q" type="text" {placeholder} class="max-w-xs" />
+</form>
