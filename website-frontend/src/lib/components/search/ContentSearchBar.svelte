d1616a091c11a2281c50a465f0f4c2b497bf6b84
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-05-16 18:14:37 +0800

    feat: add value binding to search bar in courses

--- website-frontend/src/lib/components/search/ContentSearchBar.svelte
+++ website-frontend/src/lib/components/search/ContentSearchBar.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { Input } from '$lib/@shadcn-svelte/ui/input';
+	import { page } from '$app/stores';
 
 	export let placeholder: string;
+
+	$: query = new URLSearchParams($page.url.searchParams.toString()).get('q');
 </script>
 
 <form class="flex w-full max-w-sm items-center space-x-2">
-	<Input name="q" type="text" {placeholder} class="max-w-xs" />
+	<Input name="q" type="text" {placeholder} class="max-w-xs" bind:value={query} />
 </form>
