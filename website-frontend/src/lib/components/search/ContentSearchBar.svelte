4705203b895d37234a6977c1a0e2abdb6e910557
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-05-16 18:33:23 +0800

    fix: use stores for binding search input in courses

--- website-frontend/src/lib/components/search/ContentSearchBar.svelte
+++ website-frontend/src/lib/components/search/ContentSearchBar.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { Input } from '$lib/@shadcn-svelte/ui/input';
+	import { contentSearchBarInput } from '$lib/stores';
 	import { page } from '$app/stores';
 
-	export let placeholder: string;
-
 	$: query = new URLSearchParams($page.url.searchParams.toString()).get('q');
+	$: if (query) contentSearchBarInput.set(query);
+
+	export let placeholder: string;
 </script>
 
 <form class="flex w-full max-w-sm items-center space-x-2">
-	<Input name="q" type="text" {placeholder} class="max-w-xs" bind:value={query} />
+	<Input name="q" type="text" {placeholder} class="max-w-xs" bind:value={$contentSearchBarInput} />
 </form>
--- website-frontend/src/lib/stores.ts
+++ website-frontend/src/lib/stores.ts
@@ -3,3 +3,4 @@ import { writable } from 'svelte/store';
 export const searchOpen = writable(false);
 export const mobileOpen = writable(false);
 export const searchInput = writable('');
+export const contentSearchBarInput = writable('');
