411b88aa7ab8e406233e2e37d8af84f57c6e2f73
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-05-18 07:45:41 +0800

    fix: migrate processing of home collections to server

--- website-frontend/src/routes/+page.server.ts
+++ website-frontend/src/routes/+page.server.ts
@@ -4,63 +4,6 @@ import getDirectusInstance from '$lib/directus';
 
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
-	const global = await directus.request(readSingleton('global'));
-	const home = await directus.request(
-		readSingleton('home', {
-			fields: [
-				'*',
-				{
-					featured_news: [
-						{
-							news_id: [
-								'*',
-								{
-									user_created: ['first_name', 'last_name']
-								},
-								{
-									user_updated: ['first_name', 'last_name']
-								}
-							]
-						}
-					]
-				},
-				{
-					recent_news: [
-						{
-							news_id: [
-								'*',
-								{
-									user_created: ['first_name', 'last_name']
-								},
-								{
-									user_updated: ['first_name', 'last_name']
-								}
-							]
-						}
-					]
-				},
-				{
-					recent_events: [
-						{
-							events_id: [
-								'*',
-								{
-									event_area: ['name']
-								},
-								{
-									event_tags: [
-										{
-											events_tags_id: ['name']
-										}
-									]
-								}
-							]
-						}
-					]
-				}
-			]
-		})
-	);
 	const news = await directus.request(
 		readItems('news', {
 			fields: [
@@ -75,23 +18,74 @@ export async function load({ fetch }) {
 			sort: ['-date_created']
 		})
 	);
-	const events = await directus.request(
-		readItems('events', {
-			fields: [
-				'*',
-				{
-					event_area: ['name']
-				},
-				{
-					event_tags: [
-						{
-							events_tags_id: ['name']
-						}
-					]
-				}
-			]
-		})
-	);
+	const [featured_news, recent_news, recent_events] = await (async () => {
+		const home = await directus.request(
+			readSingleton('home', {
+				fields: [
+					'*',
+					{
+						featured_news: [
+							{
+								news_id: [
+									'*',
+									{
+										user_created: ['first_name', 'last_name']
+									},
+									{
+										user_updated: ['first_name', 'last_name']
+									}
+								]
+							}
+						]
+					},
+					{
+						recent_news: [
+							{
+								news_id: [
+									'*',
+									{
+										user_created: ['first_name', 'last_name']
+									},
+									{
+										user_updated: ['first_name', 'last_name']
+									}
+								]
+							}
+						]
+					},
+					{
+						recent_events: [
+							{
+								events_id: [
+									'*',
+									{
+										event_area: ['name']
+									},
+									{
+										event_tags: [
+											{
+												events_tags_id: ['name']
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				]
+			})
+		);
+		const featured_news = home.featured_news
+			?.filter((item) => typeof item !== 'string')
+			.map((item) => item.news_id);
+		const recent_news = home.recent_news
+			?.filter((item) => typeof item !== 'string')
+			.map((item) => item.news_id);
+		const recent_events = home.recent_events
+			?.filter((item) => typeof item !== 'number')
+			.map((item) => item.events_id);
+		return [featured_news, recent_news, recent_events];
+	})();
 
-	return { global, home, news, events };
+	return { news, featured_news, recent_news, recent_events };
 }
--- website-frontend/src/routes/+page.ts
+++ website-frontend/src/routes/+page.ts
@@ -1,17 +1,9 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ data }) {
 	return {
-		title: data.global.title,
-		description: data.global.description,
 		news: data.news,
-		featured_news: data.home.featured_news
-			.filter((item) => typeof item !== 'string')
-			.map((item) => item.news_id),
-		recent_news: data.home.recent_news
-			.filter((item) => typeof item !== 'string')
-			.map((item) => item.news_id),
-		recent_events: data.home.recent_events
-			.filter((item) => typeof item !== 'number')
-			.map((item) => item.events_id)
+		featured_news: data.featured_news,
+		recent_news: data.recent_news,
+		recent_events: data.recent_events
 	};
 }
