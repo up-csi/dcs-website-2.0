64ab48fcf293ab747330b75180c7a6b2d8dc473c
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-05-29 11:31:32 +0800

    fix: migrate enhanceWysiwygContent to server

--- website-frontend/src/lib/components/flexible_content/FlexibleContent.svelte
+++ website-frontend/src/lib/components/flexible_content/FlexibleContent.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { enhanceWysiwygContent } from '.';
 	export let content: string = '';
-
-	let enhancedContent = content;
-
-	onMount(() => {
-		enhancedContent = content ? enhanceWysiwygContent(content) : '';
-	});
-
-	$: if (typeof window !== 'undefined') {
-		enhancedContent = content ? enhanceWysiwygContent(content) : '';
-	}
 </script>
 
 {#if content}
 	<div class="prose !max-w-none">
-		{@html enhancedContent}
+		{@html content}
 	</div>
 {/if}
--- website-frontend/src/lib/models-helpers.ts
+++ website-frontend/src/lib/models-helpers.ts
@@ -1,4 +1,39 @@
 import sanitize from 'sanitize-html';
 import { transform } from 'valibot';
+import { JSDOM } from 'jsdom';
 
-export const cleanHtml = transform((input: string) => sanitize(input));
+const elementClassMap = {
+	strong: 'font-bold',
+	em: 'italic',
+	img: 'rounded-xl shadow-lg max-w-full h-auto mx-auto',
+	a: 'text-blue-600 hover:text-blue-800 underline',
+	ul: 'list-disc',
+	ol: 'list-decimal',
+	blockquote: 'border-l-4 border-gray-300 italic',
+	iframe: 'rounded-xl shadow-lg mx-auto',
+	hr: 'my-8',
+	th: 'p-2 text-center',
+	td: 'p-2'
+};
+
+// TODO: Add dark mode implementation
+function enhanceWysiwygContent(htmlContent: string): string {
+	const doc = new JSDOM(htmlContent).window.document;
+
+	Object.entries(elementClassMap).forEach(([tag, className]) => {
+		const elements = doc.getElementsByTagName(tag);
+		Array.from(elements).forEach((element) => {
+			const existingClasses = element.getAttribute('class');
+			element.setAttribute(
+				'class',
+				existingClasses ? `${existingClasses} ${className}` : className
+			);
+		});
+	});
+
+	console.log(doc.body.innerHTML);
+
+	return doc.body.innerHTML;
+}
+
+export const cleanHtml = transform((input: string) => sanitize(enhanceWysiwygContent(input)));
