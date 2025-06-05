329de4b716c5194115977d3c6ac747d265a3b80c
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-06-05 14:16:11 +0800

    feat: improve mobile responsiveness for CardCarousel

--- website-frontend/src/lib/components/carousels/CardCarousel.svelte
+++ website-frontend/src/lib/components/carousels/CardCarousel.svelte
@@ -5,18 +5,12 @@
 </script>
 
 <Carousel.Root opts={{ align: 'start', dragFree: true }}>
-	<Carousel.Content class="-ml-[85%] -mr-[81%] pb-8 md:-ml-[18%] md:-mr-[17%]">
-		<Carousel.Item class="basis-1/3 md:basis-1/6">
-			<div class="h-full" />
-		</Carousel.Item>
+	<Carousel.Content class="ml-8 mr-8 pb-8">
 		{#each items as item}
-			<Carousel.Item class="basis-1/3 md:basis-1/6">
+			<Carousel.Item class="basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
 				<svelte:component this={CardComponent} {item} />
 			</Carousel.Item>
 		{/each}
-		<Carousel.Item class="basis-1/3 md:basis-1/6">
-			<div class="h-full" />
-		</Carousel.Item>
 	</Carousel.Content>
 	<Carousel.Previous
 		class="absolute -left-3 h-12 w-12 rounded-lg pl-2 text-primary-dark shadow-md disabled:opacity-0 md:h-16 md:w-20 md:pl-5"
