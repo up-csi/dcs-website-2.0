9cafecfc53326d8e08ef02b0cef43f804b9a572f
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-07-10 21:59:32 +0800

    fix: add fallback for lab hero nullable entries

--- website-frontend/src/routes/students/organizations/[slug]/+page.svelte
+++ website-frontend/src/routes/students/organizations/[slug]/+page.svelte
@@ -49,30 +49,36 @@
 			<div
 				class="space-y-9 px-4 pb-16 pt-9 md:max-w-6xl md:space-y-12 md:px-10 md:pb-20 md:pt-12 lg:pl-[302px]"
 			>
-				<div class="text-2xl font-semibold leading-tight text-primary-foreground md:leading-snug">
-					<p>{organization.mission}</p>
-				</div>
-
-				<InfoCard
-					location={organization.location ?? ''}
-					contact_email={organization.email ?? ''}
-					founding_date={organization.founding_date ?? ''}
-				/>
-
-				{#if !showFull}
-					<div class="mt-9">
-						<ReadMore bind:showFull />
+				{#if organization.mission}
+					<div class="text-2xl font-semibold leading-tight text-primary-foreground md:leading-snug">
+						<p>{organization.mission}</p>
 					</div>
-				{:else}
-					<div class="text-lg leading-normal text-primary-foreground">
+				{/if}
+
+				{#if organization.location || organization.email || organization.founding_date}
+					<InfoCard
+						location={organization.location ?? ''}
+						contact_email={organization.email ?? ''}
+						founding_date={organization.founding_date ?? ''}
+					/>
+				{/if}
+
+				{#if organization.description}
+					{#if showFull}
 						<div class="text-lg leading-normal text-primary-foreground">
-							<p class="duration-400 overflow-hidden transition-all">
-								{organization.description}
-							</p>
+							<div class="text-lg leading-normal text-primary-foreground">
+								<p class="duration-400 overflow-hidden transition-all">
+									{organization.description}
+								</p>
+							</div>
 						</div>
-					</div>
 
-					<FlexibleContent inverted content={organization.flexible_content} />
+						<FlexibleContent inverted content={organization.flexible_content} />
+					{:else}
+						<div class="mt-9">
+							<ReadMore bind:showFull />
+						</div>
+					{/if}
 				{/if}
 			</div>
 		</div>
