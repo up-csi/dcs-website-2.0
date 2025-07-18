0a5fb483370fa71944f88c168e0c56a1ac956f5b
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-07-18 21:49:30 +0800

    perf: use batch loading for Await-based loadMore in news

--- website-frontend/src/routes/news/+page.server.ts
+++ website-frontend/src/routes/news/+page.server.ts
@@ -3,13 +3,14 @@ import { aggregate, readItems, readSingleton } from '@directus/sdk';
 import getDirectusInstance from '$lib/directus';
 import { awaitAsync, parse, parseAsync, pipeAsync, promise } from 'valibot';
 import { News } from '$lib/models/news';
-import { redirect } from '@sveltejs/kit';
 import { NewsOverview } from '$lib/models/news_overview.js';
+import type { Actions } from './$types';
 
-export async function load({ fetch, url }) {
+const news_limit = 12;
+
+export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	const news_overview = parse(NewsOverview, await directus.request(readSingleton('news_overview')));
-	const news_limit = 12;
 	const news_count = await directus
 		.request(
 			aggregate('news', {
@@ -17,8 +18,6 @@ export async function load({ fetch, url }) {
 			})
 		)
 		.then((res) => parseInt(res[0].count ?? '0'));
-	if (parseInt(url.searchParams.get('limit') ?? news_limit.toString()) < news_limit)
-		throw redirect(302, url.pathname);
 	const news = parseAsync(
 		pipeAsync(promise(), awaitAsync(), News),
 		directus.request(
@@ -40,10 +39,48 @@ export async function load({ fetch, url }) {
 					}
 				],
 				sort: ['-date_created'],
-				limit: Math.max(news_limit, parseInt(url.searchParams.get('limit') ?? '0'))
+				limit: news_limit
 			})
 		)
 	);
 
 	return { news_overview, news_limit, news_count, news };
 }
+
+export const actions = {
+	loadMore: async ({ request }) => {
+		const data = await request.formData();
+		const directus = getDirectusInstance(null);
+		const news = parse(
+			News,
+			await directus.request(
+				readItems('news', {
+					fields: [
+						'*',
+						{
+							user_created: ['first_name', 'last_name']
+						},
+						{
+							user_updated: ['first_name', 'last_name']
+						},
+						{
+							news_tags: [
+								{
+									news_tags_id: ['name']
+								}
+							]
+						}
+					],
+					sort: ['-date_created'],
+					offset: parseInt((data.get('offset') ?? '0') as string),
+					limit: news_limit
+				})
+			)
+		);
+
+		return {
+			success: true,
+			items: news
+		};
+	}
+} satisfies Actions;
--- website-frontend/src/routes/news/[slug]/+page.server.ts
+++ website-frontend/src/routes/news/[slug]/+page.server.ts
@@ -1,9 +1,12 @@
 /** @type {import('./$types').PageServerLoad} */
 import { aggregate, readItems } from '@directus/sdk';
 import getDirectusInstance from '$lib/directus';
-import { error, redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { parse } from 'valibot';
 import { News, NewsItem } from '$lib/models/news';
+import type { Actions } from './$types';
+
+const other_news_limit = 12;
 
 export async function load({ url, params, fetch }) {
 	const directus = getDirectusInstance(fetch);
@@ -43,7 +46,6 @@ export async function load({ url, params, fetch }) {
 
 	const news_item = parse(NewsItem, news[0]);
 
-	const other_news_limit = 12;
 	const other_news_count = await directus
 		.request(
 			aggregate('news', {
@@ -60,8 +62,6 @@ export async function load({ url, params, fetch }) {
 			})
 		)
 		.then((res) => res[0].count);
-	if (parseInt(url.searchParams.get('limit') ?? other_news_limit.toString()) < other_news_limit)
-		throw redirect(302, url.pathname);
 	const other_news = parse(
 		News,
 		await directus.request(
@@ -88,7 +88,7 @@ export async function load({ url, params, fetch }) {
 					}
 				},
 				sort: ['-date_created'],
-				limit: Math.max(other_news_limit, parseInt(url.searchParams.get('limit') ?? '0'))
+				limit: other_news_limit
 			})
 		)
 	);
@@ -97,3 +97,46 @@ export async function load({ url, params, fetch }) {
 
 	return { link, other_news_limit, other_news_count, other_news, news_item };
 }
+
+export const actions = {
+	loadMore: async ({ request, params }) => {
+		const data = await request.formData();
+		const directus = getDirectusInstance(null);
+		const other_news = parse(
+			News,
+			await directus.request(
+				readItems('news', {
+					fields: [
+						'*',
+						{
+							user_created: ['first_name', 'last_name']
+						},
+						{
+							user_updated: ['first_name', 'last_name']
+						},
+						{
+							news_tags: [
+								{
+									news_tags_id: ['name']
+								}
+							]
+						}
+					],
+					filter: {
+						slug: {
+							_neq: params.slug
+						}
+					},
+					sort: ['-date_created'],
+					offset: parseInt((data.get('offset') ?? '0') as string),
+					limit: other_news_limit
+				})
+			)
+		);
+
+		return {
+			success: true,
+			items: other_news
+		};
+	}
+} satisfies Actions;
