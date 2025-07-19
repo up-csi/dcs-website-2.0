c28ed4e9ee4c54d5d46c2ec6e7349169ccd13bf6
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-07-19 09:09:54 +0800

    feat: add data stream and query support for Await

--- website-frontend/src/lib/components/loading/Await.svelte
+++ website-frontend/src/lib/components/loading/Await.svelte
@@ -4,22 +4,25 @@
 	import { Frown, Plus } from 'lucide-svelte';
 	import { enhance } from '$app/forms';
 	import type { ActionResult } from '@sveltejs/kit';
+	import { page } from '$app/stores';
 
 	export let onDark = false;
 	export let layout;
-	export let data: Promise<Array<unknown>>;
+	export let data: Promise<Array<object>>;
 	export let text;
 	export let component;
 	export let count;
 	export let limit: number;
 
+	$: query = new URLSearchParams($page.url.searchParams.toString());
 	$: offset = 0;
 
-	const handleLoadMore = ({ formData }: { formData: FormData }) => {
+	const handleLoadMore = async ({ formData }: { formData: FormData }) => {
+		formData.set('data', JSON.stringify(await data));
 		formData.set('offset', (offset + limit).toString());
 		return async ({ result }: { result: ActionResult }) => {
 			if (result.type === 'success' && result.data) {
-				data = Promise.all([...(await data), ...result.data.items]);
+				data = result.data.items;
 				offset = offset + limit;
 			}
 		};
@@ -48,7 +51,7 @@
 		<div class="flex flex-col items-center gap-2">
 			<div class="flex items-center justify-center gap-1">
 				{#if data.length < count}
-					<form method="POST" action="?/loadMore" use:enhance={handleLoadMore}>
+					<form method="POST" action="?/loadMore&{query.toString()}" use:enhance={handleLoadMore}>
 						<button type="submit" class="flex items-center rounded-3xl px-6 py-2 shadow-xl">
 							<Plus
 								color={onDark ? 'hsl(0, 0%, 100%)' : 'hsl(144, 69%, 24%)'}
