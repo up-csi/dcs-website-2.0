45a101bca0f6864155d800dbd4edd385890072fb
Aaron Jude Tanael <69955418+eyronjuude@users.noreply.github.com>
2025-07-20 16:11:52 +0800

    feat: use Await and lazy loading for loading search results

--- website-frontend/src/routes/search/+page.svelte
+++ website-frontend/src/routes/search/+page.svelte
@@ -3,56 +3,94 @@
 
 	import * as Tabs from '$lib/@shadcn-svelte/ui/tabs';
 	import { Search } from 'lucide-svelte';
-	import SearchResult from '$lib/components/search/SearchResult.svelte';
-	import { ScrollArea } from '$lib/@shadcn-svelte/ui/scroll-area/index.js';
 	import { page } from '$app/stores';
+	import Await from '$lib/components/loading/Await.svelte';
+	import { goto } from '$app/navigation';
+	import NewsSearchResult from '$lib/components/search/NewsSearchResult.svelte';
+	import EventsSearchResult from '$lib/components/search/EventsSearchResult.svelte';
+	import PeopleSearchResult from '$lib/components/search/PeopleSearchResult.svelte';
+	import LaboratoriesSearchResult from '$lib/components/search/LaboratoriesSearchResult.svelte';
+	import PublicationsSearchResult from '$lib/components/search/PublicationsSearchResult.svelte';
 
 	export let data;
-	let tabs;
+	const layout = 'lg:w-3/4';
 
-	$: ({ news, events, people, laboratories, publications } = data);
+	$: ({
+		news_count,
+		events_count,
+		people_count,
+		laboratories_count,
+		publications_count,
+		news,
+		events,
+		people,
+		laboratories,
+		publications
+	} = data);
 
+	$: query = new URLSearchParams($page.url.searchParams.toString());
+	$: value =
+		query.get('tab') ??
+		(news_count > 0
+			? 'News'
+			: events_count > 0
+				? 'Events'
+				: people_count > 0
+					? 'People'
+					: laboratories_count > 0
+						? 'Laboratories'
+						: publications_count > 0
+							? 'Publications'
+							: 'News');
 	$: tabs = [
-		{
-			tab: 'All',
-			res: news.length + events.length + people.length + laboratories.length + publications.length
-		},
+		// {
+		// 	tab: 'All',
+		// 	res: news_count + events_count + people_count + laboratories_count + publications_count
+		// },
 		{
 			tab: 'News',
-			res: news.length
+			res: news_count
 		},
 		{
 			tab: 'Events',
-			res: events.length
+			res: events_count
 		},
 		{
 			tab: 'People',
-			res: people.length
+			res: people_count
 		},
 		{
 			tab: 'Laboratories',
-			res: laboratories.length
+			res: laboratories_count
 		},
 		{
 			tab: 'Publications',
-			res: publications.length
+			res: publications_count
 		}
 	];
 </script>
 
-<div class="container">
-	<Tabs.Root value="All">
-		<ScrollArea orientation="horizontal">
-			<Tabs.List class="mt-20 flex w-full items-center justify-center bg-background">
-				{#each tabs as { tab, res }}
-					<Tabs.Trigger
-						value={tab}
-						class="focus:ring-none rounded-none data-[state=active]:shadow-none lg:data-[state=active]:border-b-2"
-						>{tab} ({res})</Tabs.Trigger
-					>
-				{/each}
-			</Tabs.List>
-		</ScrollArea>
+<div class="container p-8">
+	<Tabs.Root
+		bind:value
+		onValueChange={(value) => {
+			// if (value == 'All') query.delete('tab');
+			// else query.set('tab', value ?? '');
+			query.set('tab', value ?? '');
+			goto(`?${query.toString()}`, { noScroll: true });
+		}}
+	>
+		<Tabs.List
+			class="flex h-full w-full flex-wrap items-center justify-center border bg-background lg:mt-10"
+		>
+			{#each tabs as { tab, res }}
+				<Tabs.Trigger
+					value={tab}
+					class="focus:ring-none rounded-none data-[state=active]:shadow-none lg:data-[state=active]:border-b-2"
+					>{tab} ({res})</Tabs.Trigger
+				>
+			{/each}
+		</Tabs.List>
 
 		{#each tabs as { tab, res }}
 			<Tabs.Content value={tab}>
@@ -61,130 +99,87 @@
 						<p class="text-3xl font-bold">
 							{res} Search result{#if res !== 1}s{/if} for
 							<span class="font-normal">{$page.url.searchParams.get('q')}</span>
-							{#if tab !== 'All'}
+							<span>in {tab}</span>
+							<!-- {#if tab !== 'All'}
 								<span>in {tab}</span>
-							{/if}
+							{/if} -->
 						</p>
 					</div>
-
-					{#if tab === 'All'}
-						{#each news as { slug, title, summary, background_image }}
-							<SearchResult href="/news/{slug}" image={background_image} name={title}>
-								<p class="text-xl font-bold">{title}</p>
-								{#if summary}
-									<p class="line-clamp-2">{@html summary}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-						{#each events as { slug, event_headline, event_content, hero_image }}
-							<SearchResult href="/events/{slug}" image={hero_image} name={event_headline}>
-								<p class="text-xl font-bold">{event_headline}</p>
-								{#if event_content}
-									<p class="line-clamp-2">{@html event_content}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-						{#each people as { username, first_name, last_name, profile_image, position }}
-							<SearchResult
-								href="/people/{username}"
-								image={profile_image}
-								name="{first_name} {last_name}"
-							>
-								<p class="text-xl font-bold">{first_name} {last_name}</p>
-								{#if position}
-									<p>{position}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-						{#each laboratories as { slug, name, description, logo }}
-							<SearchResult href="/research/labs/{slug}" image={logo} {name}>
-								<p class="text-xl font-bold">{name}</p>
-								{#if description}
-									<p class="line-clamp-2">{description}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-						{#each publications as publication}
-							<SearchResult
-								{publication}
-								href="/research/publications"
-								image={publication.hero_image}
-								name={publication.title}
-							>
-								<p class="text-xl font-bold">{publication.title}</p>
-								{#if publication.authors.length > 0}
-									<p>
-										{#each publication.authors as { last_name }, i}
-											{last_name}{#if i + 1 !== publication.authors.length}{`, `}{/if}
-										{/each}
-									</p>
-								{/if}
-								{#if publication.abstract}
-									<p class="line-clamp-2">{publication.abstract}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-					{:else if tab === 'News'}
-						{#each news as { slug, title, summary, background_image }}
-							<SearchResult href="/news/{slug}" image={background_image} name={title}>
-								<p class="text-xl font-bold">{title}</p>
-								{#if summary}
-									<p class="line-clamp-2">{@html summary}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-					{:else if tab === 'Events'}
-						{#each events as { slug, event_headline, event_content, hero_image }}
-							<SearchResult href="/events/{slug}" image={hero_image} name={event_headline}>
-								<p class="text-xl font-bold">{event_headline}</p>
-								{#if event_content}
-									<p class="line-clamp-2">{@html event_content}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-					{:else if tab === 'People'}
-						{#each people as { username, first_name, last_name, profile_image, position }}
-							<SearchResult
-								href="/people/{username}"
-								image={profile_image}
-								name="{first_name} {last_name}"
-							>
-								<p class="text-xl font-bold">{first_name} {last_name}</p>
-								{#if position}
-									<p>{position}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-					{:else if tab === 'Laboratories'}
-						{#each laboratories as { slug, name, description, logo }}
-							<SearchResult href="/research/labs/{slug}" image={logo} {name}>
-								<p class="text-xl font-bold">{name}</p>
-								{#if description}
-									<p class="line-clamp-2">{description}</p>
-								{/if}
-							</SearchResult>
-						{/each}
-					{:else if tab === 'Publications'}
-						{#each publications as publication}
-							<SearchResult
-								{publication}
-								href="/research/publications"
-								image={publication.hero_image}
-								name={publication.title}
-							>
-								<p class="text-xl font-bold">{publication.title}</p>
-								{#if publication.authors.length > 0}
-									<p>
-										{#each publication.authors as { last_name }, i}
-											{last_name}{#if i + 1 !== publication.authors.length}{`, `}{/if}
-										{/each}
-									</p>
-								{/if}
-								{#if publication.abstract}
-									<p class="line-clamp-2">{publication.abstract}</p>
-								{/if}
-							</SearchResult>
-						{/each}
+					<!-- {#if tab === 'All'}
+						<Await {layout} data={news} text="news" component={NewsSearchResult} count={news_count} />
+						<Await
+							{layout}
+							data={events}
+							text="events"
+							component={EventsSearchResult}
+							count={events_count}
+						/>
+						<Await
+							{layout}
+							data={people}
+							text="people"
+							component={PeopleSearchResult}
+							count={people_count}
+						/>
+						<Await
+							{layout}
+							data={laboratories}
+							text="laboratories"
+							component={LaboratoriesSearchResult}
+							count={laboratories_count}
+						/>
+						<Await
+							{layout}
+							data={publications}
+							text="publications"
+							component={PublicationsSearchResult}
+							count={publications_count}
+						/>
+					{/if} -->
+					{#if tab === 'News'}
+						<Await
+							{layout}
+							data={news}
+							text="news"
+							component={NewsSearchResult}
+							count={news_count}
+						/>
+					{/if}
+					{#if tab === 'Events'}
+						<Await
+							{layout}
+							data={events}
+							text="events"
+							component={EventsSearchResult}
+							count={events_count}
+						/>
+					{/if}
+					{#if tab === 'People'}
+						<Await
+							{layout}
+							data={people}
+							text="people"
+							component={PeopleSearchResult}
+							count={people_count}
+						/>
+					{/if}
+					{#if tab === 'Laboratories'}
+						<Await
+							{layout}
+							data={laboratories}
+							text="laboratories"
+							component={LaboratoriesSearchResult}
+							count={laboratories_count}
+						/>
+					{/if}
+					{#if tab === 'Publications'}
+						<Await
+							{layout}
+							data={publications}
+							text="publications"
+							component={PublicationsSearchResult}
+							count={publications_count}
+						/>
 					{/if}
 				{:else}
 					<div class="relative h-[50vh]">
@@ -192,12 +187,13 @@
 							<Search class="h-[30vh] w-[30vh] text-secondary/25" />
 						</div>
 						<div class="absolute left-0 top-0 flex h-full w-full items-center justify-center">
-							<p>
+							<p class="text-center">
 								{#if $page.url.searchParams.get('q')}
 									No search results for <b>{$page.url.searchParams.get('q')}</b>
-									{#if tab !== 'All'}
+									<span>in {tab}</span>
+									<!-- {#if tab !== 'All'}
 										<span>in {tab}</span>
-									{/if}
+									{/if} -->
 								{:else}
 									Please provide a search input
 								{/if}
